042d1b862335ecfe4f561d95d0ab558f24ec4ecb
MehdiMitiche <gm_mitiche@esi.dz>
2021-01-06 01:15:17 +0100

    function save_charts_html added successfully !

--- helpers.py
+++ helpers.py
@@ -82,3 +82,83 @@ def check_categorical_var(data):
     if(len(data.unique()) > 10):
         return False
     return True
+
+
+def saveChartsOnHTML(folders):
+    """
+    Function that display all the charts of the project in one HTML page
+
+    :param folders: the list of the folder names containing the charts we want to display
+    :return:a string
+    """
+    style = """
+        <style>
+        *{
+            padding:0px;
+            margin:0px;
+            font-family: 'Oswald', sans-serif;
+        }
+        .title{
+            height:11vh;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            font-size:32px;
+            font-weight:600;
+            background-color:#f5fafd
+        }
+        .section-title{
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            font-size:25px;
+            font-weight:600;
+            margin-top:50px
+
+        }
+        .container{
+            display:flex;
+            flex-wrap:wrap;
+            justify-content:center;
+        }
+        img{
+            margin:10px 5px
+        }
+        @media only screen and (max-width: 700px) {
+            img {
+                width:80vw;
+                height:80vw;
+            }
+        }
+        </style>
+    """
+    body = ""
+    for folder in folders:
+        images = ""
+        for img in os.listdir(f"./charts/{folder}"):
+            images += f'<img src="./charts/{folder}/{img}"   />'
+        body += f'<div class="section-title">{folder}</div><div class="container">{images}</div>'
+    text = f'''
+    <html>
+        <head>
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>PETALE Charts</title>
+            {style}
+        </head>
+        <body>
+            <div class="title">Petal Charts</div>
+            
+            {body}
+            
+        </body>
+    </html>
+    '''
+
+    file = open("PETALE_Charts.html", "w")
+    file.write(text)
+    file.close()
+
+
+saveChartsOnHTML(["charts_Cardio_4_Test de Marche de 6 Minutes (TDM6)"])

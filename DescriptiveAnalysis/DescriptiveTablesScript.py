1be769dd8f1a988fca3e6830d5175fd88c57011d
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2021-01-14 13:44:36 -0500

    New files for descriptive analysis

---
+++ DescriptiveAnalysis/DescriptiveTablesScript.py
@@ -0,0 +1,7 @@
+"""
+Authors : Nicolas Raymond
+          Mehdi Mitiche
+
+This file store the procedures that need to be executed in order to get descriptive tables
+as illustrated in GeneralTableTemplate.pdf and TDM6TableTemplate.pdf
+"""

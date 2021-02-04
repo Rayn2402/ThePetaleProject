9d6a50bf5c6477c31f9f8fd686f836c1eb138c94
mehdiMitiche <gm_mitiche@esi.dz>
2021-02-04 09:40:51 +0100

    AnalyticalLinearRegression class updated

--- Models/LinearRegression/model.py
+++ Models/LinearRegression/model.py
@@ -1,20 +1,61 @@
-import sys
 import numpy as np
-sys.path.append("../..")
-from Metrics.metric import RMSE
 
 class AnalyticalLinearRegression():
-    def __init__(self, df, features, dep_var):
+    def __init__(self, data, features, target):
+        """
+        class that will represent the analytical solution of the linear regression
+        Parameters
+        ----------
+        data: pandas data frame
+            The data frame object for the input data. It must
+            contain all the features and the target column
+        features: List of strings
+            The names of the feature columns in the data.
+        target: string
+            The name of the output variable column in the data
+            provided.
+        """
+        #we extract and read the data as numpy arrays **TO BE CONVERTED TO TENSORS
+        y = data[target].values.astype(float)
+        x = data[features].values.astype(float)
+        #we add a columns containing only ones
+        x = np.concatenate((np.ones((x.shape[0],1)), x),axis = 1)
+        #we save the needed parameters
+        self.target = target
+        self.features = features
+        self.b = np.linalg.pinv(x.T.dot(x)).dot(x.T).dot(y)
 
-        x_train = df[features].values.astype(float)
-        y_train = df[dep_var].values.astype(float)
+    def predict(self, x_data):
+        """
+        function that returns the predictions of a given data
+        """
+        x_data = x_data[self.features].values.astype(float)
+        x_data = np.concatenate((np.ones((x_data.shape[0],1)), x_data),axis = 1)
+        return x_data.dot(self.b)
 
-        self.b = np.linalg.pinv(x_train.T.dot(x_train)).dot(x_train.T).dot(y_train)
-    
-    def test(self, x, y):
+    def test(self, data):
+        """
+        function that tests the model by returning the error of the prediction of a given data
+        """
+        #we extract the data needed
+        y = data[self.target].values.astype(float)
+        x = data[self.features].values.astype(float)
+        #we add a column comtaining omly ones
+        x = np.concatenate((np.ones((x.shape[0],1)), x),axis = 1)
+        #we make our prediction
         y_pred = x.dot(self.b)
-        return RMSE(y_pred, y)
+        #we return the metric
+        return self.rmse(y_pred, y)
     
-    def predict(self, x):
-        return x.dot(self.b)
+    def get_solution(self):
+        """
+        function that returns the solution of the linear regression
+        """
+        return self.b
+
+    def rmse(self, y_pred, y):
+        """
+        functiom that return the Root Mean Squared Error
+        """
+        return np.sqrt((np.square(y_pred - y).sum()) /y.shape[0])
 

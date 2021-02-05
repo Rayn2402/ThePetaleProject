13db451f6799a623f5f6703c2aba6f0f63a0b61e
mehdiMitiche <gm_mitiche@esi.dz>
2021-02-05 21:42:35 +0100

    treatment in the linearRegressor converted to pytorch tensors

--- model/LinearModel.py
+++ model/LinearModel.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 #To be converted to tensors instead of numpy arrays
+
 class LinearRegressor():
-    def __init__(self, data, features, target):
+    def __init__(self, data, cont_cols, target):
         """
-        class that will represent the analytical solution of the linear regression
+        the class that will represent the analytical solution of the linear regression
         Parameters
         ----------
         data: pandas data frame
@@ -17,46 +18,46 @@ class LinearRegressor():
             The name of the output variable column in the data
             provided.
         """
-        #we extract and read the data as numpy arrays 
-        y = data[target].values.astype(float)
-        x = data[features].values.astype(float)
+        #we extract and read the data as torch tensors
+        x, y = torch.from_numpy(data[cont_cols].values.astype(float)), torch.from_numpy(data[target].values.astype(float))
         #we add a columns containing only ones
-        x = np.concatenate((np.ones((x.shape[0],1)), x),axis = 1)
+        x = torch.cat([torch.ones(x.shape[0], 1) , x], dim=1)
         #we save the needed parameters
         self.target = target
-        self.features = features
-        self.b = np.linalg.pinv(x.T.dot(x)).dot(x.T).dot(y)
-
+        self.cont_cols = cont_cols
+        self.b = torch.matmul(torch.matmul(torch.inverse( torch.matmul(torch.transpose(x, 0, 1), x)), torch.transpose(x, 0, 1)), y )
     def predict(self, x_data):
         """
         function that returns the predictions of a given data
         """
-        x_data = x_data[self.features].values.astype(float)
-        x_data = np.concatenate((np.ones((x_data.shape[0],1)), x_data),axis = 1)
-        return x_data.dot(self.b)
-
+        x_data = torch.from_numpy(x_data[self.cont_cols].values.astype(float))
+        x_data = torch.cat([torch.ones(x_data.shape[0], 1), x_data], dim=1)
+        return  torch.matmul(x, self.b)
     def test(self, data):
         """
         function that tests the model by returning the error of the prediction of a given data
         """
         #we extract the data needed
-        y = data[self.target].values.astype(float)
-        x = data[self.features].values.astype(float)
+        x, y = torch.from_numpy(data[self.cont_cols].values.astype(float)), torch.from_numpy(data[self.target].values.astype(float))
         #we add a column comtaining omly ones
-        x = np.concatenate((np.ones((x.shape[0],1)), x),axis = 1)
+        x = torch.cat([torch.ones(x.shape[0], 1) , x], dim=1)
         #we make our prediction
-        y_pred = x.dot(self.b)
+        y_pred = torch.matmul(x, self.b)
         #we return the metric
         return self.rmse(y_pred, y)
-    
     def get_solution(self):
         """
         function that returns the solution of the linear regression
         """
         return self.b
-
     def rmse(self, y_pred, y):
         """
         functiom that returns the Root Mean Squared Error
         """
-        return np.sqrt((np.square(y_pred - y).sum()) /y.shape[0])
+        #we caclulate the root mean square error
+        return torch.sqrt(torch.mean(torch.square(y_pred - y))).item()
+
+
+class GDLinearRegressor():
+    def __init__(self):
+        

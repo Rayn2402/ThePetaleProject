b6e948dffcd6330d171b52238fb6b22411c28bbc
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-07 14:26:04 -0500

    New script for table with preselected VO2 ID

---
+++ SQL/NewTablesScripts/VO2_ID_FILTERED_Script.py
@@ -0,0 +1,41 @@
+"""
+Author : Nicolas Raymond
+
+This file contains the procedure to create the table with IDs of
+participant that satisfies criteria of maximal effort.
+
+IDs were sent by Maxime Caru
+
+Criterias can be found in :
+
+"Maximal cardiopulmonary exercise testing in childhood acute
+ lymphoblastic leukemia survivors exposed to chemotherapy"
+
+"""
+from SQL.DataManager.Utils import PetaleDataManager
+from pandas import read_csv
+
+
+def fill_id(id):
+    """
+    Add characters missing to ID
+    :param id: current id (string)
+    """
+    return f"P" + "".join(["0"]*(3-len(id))) + id
+
+
+TABLE_NAME = "VO2_ID"
+COL = "Participant"
+
+if __name__ == '__main__':
+
+    # We build a PetaleDataManager that will help interacting with PETALE database
+    user_name = input("Enter your username to access PETALE database : ")
+    data_manager = PetaleDataManager(user_name)
+
+    # We build the pandas dataframe
+    IDs = read_csv(f"{TABLE_NAME}.csv")
+    IDs[COL] = IDs[COL].astype('string').apply(fill_id)
+
+    # We create and fill the table in the database
+    data_manager.create_and_fill_table(IDs, TABLE_NAME, {COL: "text"})

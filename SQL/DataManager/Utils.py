e24d67623292dbd88b188162a88e4aa3fa6dc289
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-07 11:09:35 -0500

    import lines changed

--- SQL/DataManager/Utils.py
+++ SQL/DataManager/Utils.py
@@ -6,8 +6,8 @@ Authors : Nicolas Raymond
 This file contains all functions linked to SQL data management
 
 """
-import ChartServices as ChartServices
-import Helpers as Helpers
+from . import ChartServices as ChartServices
+from . import Helpers as Helpers
 import psycopg2
 import pandas as pd
 import os

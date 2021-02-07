3ffdc0f9553c1d892ab2facb33608477f8aef001
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-07 11:03:38 -0500

    Preprocessing functions added and transforms removed

--- Datasets/Preprocessing.py
+++ Datasets/Preprocessing.py
@@ -4,66 +4,24 @@ Authors : Nicolas Raymond
 This files contains all class function related to preprocessing
 
 """
-import pandas as pd
+from .Transforms import ContinuousTransform as ConT
+from .Transforms import CategoricalTransform as CaT
 
 
-class ContinuousTransform:
+def preprocess_continuous(df):
     """
-    Class of transformation that can be applied to continuous data
+    Applies all continuous transforms to a dataframe containing only continuous data
+
+    :param df: pandas dataframe
+    :return: pandas dataframe
     """
-    type = "cont"
-
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
+    return ConT.normalize(ConT.fill_missing(ConT.to_float(df)))
 
 
-class CategoricalTransform:
+def preprocess_categoricals(df):
     """
-    Class of transformation that can be applied to categorical data
+    Applies all categorical transforms to a dataframe containing only continuous data
+    :param df: pandas dataframe
+    :return: pandas dataframe, list of encoding sizes
     """
-    type = "cat"
-
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
-
-
-class ToFloat(ContinuousTransform):
-    """
-    Changes type of pandas columns to float
-    """
-    def __call__(self, df):
-        return df.astype('float')
-
-
-class Normalize(ContinuousTransform):
-    """
-    Applies normalization to columns of a pandas dataframe
-    """
-    def __call__(self, df):
-        return (df-df.mean())/df.std()
-
-
-class FillMissing(ContinuousTransform):
-    """
-    Fills missing value of continuous data columns with mean
-    """
-    def __call__(self, df):
-        return df.fillna(df.mean())
-
-
-class ToCategory(CategoricalTransform):
-    """
-    Changes type of pandas column to category
-    """
-    def __call__(self, df):
-        return df.astype('category')
-
-
-class OneHotEncode(CategoricalTransform):
-    """
-    Replaces category levels with new binary columns
-    """
-    def __call__(self, df):
-        encoding_sizes = [len(df[col].cat.categories) for col in df.columns]
-        return pd.get_dummies(df), encoding_sizes
-
+    return CaT.one_hot_encode(CaT.to_category(df))

e8119eba208b32109d6f915998a92d5d6c8034d4
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-07 15:46:09 -0500

    Participant IDs added to dataset and load_warmup_dataset function added

--- Datasets/Datasets.py
+++ Datasets/Datasets.py
@@ -13,16 +13,21 @@ from .Transforms import ContinuousTransform as ConT
 
 class PetaleDataset(Dataset):
 
-    def __init__(self, df, cont_cols, target, cat_cols=None):
+    def __init__(self, df, cont_cols, target, id, cat_cols=None, split=True):
         """
         Creates a petale dataset where categoricals columns are separated from the continuous by default.
 
         :param df: pandas dataframe
         :param cont_cols: list with names of continuous columns
         :param target: string with target column name
+        :param id: string with ids column name
         :param cat_cols: list with names of categorical columns
+        :param split: boolean indicating if categorical features must remain separated from continuous features
         """
+        if id not in df.columns:
+            raise Exception('IDs missing from the dataframe')
 
+        self.IDs = df[id]
         self.X_cont = preprocess_continuous(df[cont_cols])
         self.X_cont = from_numpy(self.X_cont.values)
 
@@ -32,6 +37,9 @@ class PetaleDataset(Dataset):
 
         self.y = from_numpy(ConT.to_float(df[target]).values).flatten()
         self.getter = self.define_getter(cat_cols)
+
+        if (not split) and cat_cols is not None:
+            self.__concat_dataset()
 
     def __len__(self):
         return self.X_cont.shape[0]
@@ -56,7 +64,7 @@ class PetaleDataset(Dataset):
 
         return f
 
-    def concat_dataset(self):
+    def __concat_dataset(self):
         """
         Concatenates categorical and continuous data
         WARNING! : !Categorical and continuous must remain separate if we want to have an embedding layer!
@@ -66,5 +74,20 @@ class PetaleDataset(Dataset):
         self.getter = self.define_getter(None)
 
 
+def load_warmup_dataset(dm, split=True):
+    """
+    Loads 'Learning_0_6MWT_and_Generals (WarmUp)' table and create a PetaleDataset object
 
+    :param dm: PetaleDataManager
+    :param split: Boolean indicating if we want to split categorical variables from the continuous ones
+    :return: PetaleDataset
+    """
+    # We save some important constants
+    CONTINUOUS_COL = ['34503 Weight', '35149 TDM6_HR_6_2', '35142 TDM6_Distance_2',
+                      'Duration of treatment', 'Age', 'MVLPA']
+    ID = "Participant"
+    TARGET = '35009 EE_VO2r_max'
 
+    df = dm.get_table('Learning_0_6MWT_and_Generals (WarmUp)')
+
+    return PetaleDataset(df, CONTINUOUS_COL, TARGET, ID, split=split)

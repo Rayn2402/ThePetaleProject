4d8eb7cf2444b1ced4ab4594f1b5c843e3ab2f42
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-08 12:07:15 -0500

    MAE added to regression metrics

--- Utils/score_metrics.py
+++ Utils/score_metrics.py
@@ -3,7 +3,7 @@ Author : Nicolas Raymond
 
 This file contains metric used to measure models' performances
 """
-from torch import sqrt
+from torch import sqrt, abs
 
 
 class RegressionMetrics:
@@ -12,6 +12,7 @@ class RegressionMetrics:
     def pearson(pred, targets):
         """
         Computes the pearson correlation coefficient between predictions and targets
+        NOTE! : A strong correlation does not implies good accuracy
 
         :param pred: (N,) tensor
         :param targets: (N,) tensor
@@ -21,6 +22,17 @@ class RegressionMetrics:
         t = targets - targets.mean()
 
         return p.dot(t) / (sqrt((p**2).sum())*sqrt((t**2).sum()))
+
+    @staticmethod
+    def mean_absolute_error(pred, targets):
+        """
+        Computes the mean absolute error between pred and targets
+
+        :param pred: (N,) tensor
+        :param targets: (N,) tensor
+        :return: (1,) tensor
+        """
+        return abs(pred - targets).mean()
 
 
 class ClassificationMetrics:

78ffa8a12bbc6f1ed0cda6cc63cdf4de9a3ab609
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-08 15:09:43 -0500

    embedding visualization function added

--- Utils/visualization.py
+++ Utils/visualization.py
@@ -5,6 +5,7 @@ This file contains all function related to data visualization
 
 """
 from matplotlib import pyplot as plt
+from sklearn.manifold import TSNE
 
 
 def compare_predictions(preds, targets, title=None):
@@ -12,6 +13,7 @@ def compare_predictions(preds, targets, title=None):
     Compares predictions to targets in a 2D scatter plot
 
     :param preds: tuple of (N,) tensors
+    :param title: string to add as title of the plot
     :param targets: (N,) tensor
     """
     if isinstance(preds, tuple):
@@ -25,3 +27,28 @@ def compare_predictions(preds, targets, title=None):
 
     plt.show()
     plt.close()
+
+
+def visualize_embeddings(embeddings, category_levels, perplexity=10, title=None):
+    """
+    Visualizes embeddings in a 2D space
+
+    :param embeddings: (NxD) tensor
+    :param category_levels: (N,) tensor (with category indices)
+    :param perplexity: (int) perplexity parameter of TSNE
+    :param title: string to add as title of the plot
+    """
+    X = embeddings.numpy()
+    y = category_levels.numpy()
+
+    if X.shape[1] > 2:
+        X = TSNE(n_components=2, perplexity=perplexity).fit_transform(X)
+
+    plt.scatter(X[:, 0], X[:, 1], c=y)
+
+    if title is not None:
+        plt.title(title)
+
+    plt.show()
+    plt.close()
+

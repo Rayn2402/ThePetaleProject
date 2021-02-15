656cc2ba96c7cdee4b3ccac346733c3bb17bd7b1
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-15 14:14:48 -0500

    Sampling corrected and split_train_test added

--- Datasets/Sampling.py
+++ Datasets/Sampling.py
@@ -9,10 +9,10 @@ import pandas as pd
 
 def stratified_sample(df, target_col, n, quantiles=4):
     """
-    Proceeds to a stratified sampling of the original dataset
+    Proceeds to a stratified sampling of the original dataset based on the target variable
 
     :param df: pandas dataframe
-    :param target_col: name of the column to use
+    :param target_col: name of the column to use for stratified sampling
     :param n: sample size, if 0 < n < 1 we consider n as a percentage of data to select
     :param quantiles: number of quantiles to used if the target_col is continuous
     :return: pandas dataframe
@@ -26,14 +26,34 @@ def stratified_sample(df, target_col, n, quantiles=4):
     elif 0 < n < 1:
         n = int(n*df.shape[0])
 
+    # We make a deep copy of the current dataframe
+    sample = df.copy()
+
+    # If the column on which we want to do a stratified sampling is continuous,
+    # we create another discrete column based on quantiles
     if len(df[target_col].unique()) > 10:
-        df["quantiles"] = pd.qcut(df[target_col], quantiles, labels=False)
+        sample["quantiles"] = pd.qcut(sample[target_col], quantiles, labels=False)
         target_col = "quantiles"
 
-    sample = df.groupby(target_col, group_keys=False).\
-        apply(lambda x: x.sample(int(np.rint(n*len(x)/len(df))))).sample(frac=1).reset_index(drop=True)
+    # We execute the sampling
+    sample = sample.groupby(target_col, group_keys=False).\
+        apply(lambda x: x.sample(int(np.rint(n*len(x)/len(sample))))).sample(frac=1)
 
     sample = sample.drop(['quantiles'], axis=1, errors='ignore')
-    df = df.drop(['quantiles'], axis=1, errors='ignore')
 
     return sample
+
+
+def split_train_test(df, target_col, test_size=0.15):
+    """
+    Split de training and testing data contained within a pandas dataframe
+    :param df: pandas dataframe
+    :param target_col: name of the target column
+    :param test_size: number of elements in the test set (if 0 < test_size < 1 we consider the parameter as a %)
+    :return: 2 pandas dataframe
+    """
+    test_data = stratified_sample(df, target_col, test_size)
+    train_data = df.drop(test_data.index)
+
+    return train_data, test_data
+

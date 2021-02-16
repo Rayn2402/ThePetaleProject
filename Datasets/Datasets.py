ac663033b57168feaa206d63964171ee3df1c719
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-02-15 20:16:13 -0500

    Normalization of test data now done with mean and std from train

--- Datasets/Datasets.py
+++ Datasets/Datasets.py
@@ -9,12 +9,14 @@ from torch.utils.data import Dataset
 from torch import from_numpy, cat, ones
 from .Preprocessing import *
 from .Transforms import ContinuousTransform as ConT
+from .Sampling import split_train_test
 from SQL.NewTablesScripts.constants import *
 
 
 class PetaleDataset(Dataset):
 
-    def __init__(self, df, cont_cols, target, id, cat_cols=None, split=True, add_biases=False):
+    def __init__(self, df, cont_cols, target, id, cat_cols=None,
+                 split=True, add_biases=False, mean=None, std=None):
         """
         Creates a petale dataset where categoricals columns are separated from the continuous by default.
 
@@ -25,6 +27,8 @@ class PetaleDataset(Dataset):
         :param cat_cols: list with names of categorical columns
         :param split: boolean indicating if categorical features must remain separated from continuous features
         :param add_biases: boolean indicating if a column of ones should be added at the beginning of X_cont
+        :param mean: means to use for data normalization (pandas series)
+        :param std : stds to use for data normalization (pandas series)
         """
         if id not in df.columns:
             raise Exception('IDs missing from the dataframe')
@@ -33,7 +37,7 @@ class PetaleDataset(Dataset):
         self.IDs = df[id]
 
         # We save and preprocess continuous features
-        self.X_cont = preprocess_continuous(df[cont_cols])
+        self.X_cont = preprocess_continuous(df[cont_cols], mean, std)
         self.X_cont = from_numpy(self.X_cont.values)
 
         # We save the number of elements in the datasets
@@ -96,17 +100,36 @@ class PetaleDataset(Dataset):
         self.X_cat = None
 
 
-def load_warmup_dataset(dm, split=True, add_biases=False):
+def load_warmup_dataset(dm, test_size=0.15, split_cat=True, add_biases=False):
     """
     Loads 'Learning_0' table and create a PetaleDataset object
 
     :param dm: PetaleDataManager
-    :param split: Boolean indicating if we want to split categorical variables from the continuous ones
+    :param test_size: number of elements in the test set (if 0 < test_size < 1 we consider the parameter as a %)
+    :param split_cat: Boolean indicating if we want to split categorical variables from the continuous ones
     :param add_biases: boolean indicating if a column of ones should be added at the beginning of X_cont
-    :return: PetaleDataset
+    :return: 2 PetaleDataset
     """
     # We save some important constants
     continuous_columns = [WEIGHT, TDM6_HR_END, TDM6_DIST, DT, AGE, MVLPA]
+
+    # We load the data
     df = dm.get_table(LEARNING_0)
 
-    return PetaleDataset(df, continuous_columns, target=VO2R_MAX, id=PARTICIPANT, split=split, add_biases=add_biases)
+    # We make sure that continuous variables are considered as continuous
+    df[continuous_columns] = ConT.to_float(df[continuous_columns])
+
+    # We split the training and test data
+    train, test = split_train_test(df, VO2R_MAX, test_size)
+
+    # We save the mean and the standard deviations of the continuous columns in train
+    mean, std = train[continuous_columns].mean(), train[continuous_columns].std()
+
+    # We create the test and train datasets
+    train_ds = PetaleDataset(train, continuous_columns, VO2R_MAX, PARTICIPANT,
+                             split=split_cat, add_biases=add_biases)
+
+    test_ds = PetaleDataset(test, continuous_columns, VO2R_MAX, PARTICIPANT,
+                            split=split_cat, mean=mean, std=std, add_biases=add_biases)
+
+    return train_ds, test_ds

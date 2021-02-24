35b3f2927ffd22fbf394a33edf1c9ed61afbaaf1
mehdiMitiche <gm_mitiche@esi.dz>
2021-02-24 10:06:29 +0100

    batch size max updated

--- Config/Config.py
+++ Config/Config.py
@@ -12,7 +12,7 @@ METRICS = ["MSE", "ACCURACY"]
 HYPER_PARAMS = {
     "lr":{"min":0.0001,"max":0.1},
     "optimizer_name":{"values": ["Adam","RMSprop","SGD"]},
-    "batch_size":{"min":8, "max":128, "step":8 },
+    "batch_size":{"min":5, "max":100, "step":5 },
     "n_layers":{"min":0,"max":1},
     "n_units":{"min":1,"max":100},
     "dropout":{"min":0,"max":1}, 

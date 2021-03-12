bcbef6a4d24e7ed945538d15417c1bd4e365e02f
mehdiMitiche <gm_mitiche@esi.dz>
2021-03-12 18:55:26 -0500

    BatchNorm Problem fixed

--- Training/Training.py
+++ Training/Training.py
@@ -56,7 +56,10 @@ class Trainer:
             batch_size = train_set.__len__()
 
         # we create the the train data loader
-        train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+        if len(train_set) % batch_size == 1:
+            train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=True)
+        else:
+            train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
         # we create the the validation data loader
         val_loader = DataLoader(val_set, batch_size=val_set.__len__())

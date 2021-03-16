0f5dcd2a175629950c7b6b680e5b2fa744d38335
Mehdi Mitiche <gm_mitiche@esi.dz>
2021-03-16 18:32:52 -0400

    Evaluator classes updated to be more general

--- Evaluator/Evaluator.py
+++ Evaluator/Evaluator.py
@@ -40,6 +40,52 @@ class Evaluator:
         self.direction = direction
         self.seed = seed
 
+    def nested_cross_valid(self):
+        """
+        Method to call when we want to perform a nested cross validation to evaluate a model
+
+        :return: the scores of the model after performing a nested cross validation
+        """
+
+        # We get all the train, test, inner train, qnd inner test sets with our sampler
+        all_datasets = self.sampler(k=self.k, l=self.l)
+
+        # We init the list that will contains the scores
+        scores = []
+
+        for i in range(self.k):
+
+            # We the get the train and the test datasets
+            train_set, test_set, valid_set = self.get_datasets(all_datasets[i])
+
+            # We create the tuner to perform the hyperparameters optimisation
+            tuner = self.create_tuner(datasets=all_datasets[i]["inner"])
+
+            # We perform the hyper parameters tuning to get the best hyper parameters
+            best_hyper_params = tuner.tune()
+
+            # We create our model with the best hyper parameters
+            model = self.create_model(best_hyper_params=best_hyper_params)
+
+            # We create a trainer to train the model
+            trainer = self.create_trainer(model=model, best_hyper_params=best_hyper_params)
+
+            # We train our model with the best hyper parameters
+            trainer.fit(train_set=train_set, val_set=valid_set)
+
+            # We extract x_cont, x_cat and target from the validset
+            x_cont = test_set.X_cont
+            target = test_set.y
+            if test_set.X_cat is not None:
+                x_cat = test_set.X_cat
+            else:
+                x_cat = None
+
+            # we calculate the score with the help of the metric function
+            scores.append(self.metric(trainer.predict(x_cont, x_cat).float(), target))
+
+        return sum(scores) / len(scores)
+
 
 class NNEvaluator(Evaluator):
     def __init__(self, model_generator, sampler, hyper_params, n_trials, metric, k, l, max_epochs=100,
@@ -55,62 +101,54 @@ class NNEvaluator(Evaluator):
 
         self.max_epochs = max_epochs
 
-    def nested_cross_valid(self):
+    def get_datasets(self, dataset_dictionary):
         """
-        Method to call when we want to perform a nested cross validation and evaluate a Neural Network model
+        Method to extract the train, test, and valid sets
 
-        :return: the scores of the model after performing a nested cross validation
+        :param dataset_dictionary: Python dictionary that contains the three sets
+
+        """
+        return dataset_dictionary["train"], dataset_dictionary["test"], dataset_dictionary["valid"]
+
+    def create_tuner(self, datasets):
+        """
+        Method to create the Tuner object that will be used in the hyper parameters tuning
+
+        :param datasets: Python list that contains all the inner train, inner test, amd inner valid sets
+
         """
 
-        # we get all the train, test, inner train, qnd inner test sets with our sampler
-        all_datasets = self.sampler(k=self.k, l=self.l)
+        return NNTuner(model_generator=self.model_generator, datasets=datasets,
+                       hyper_params=self.hyper_params, n_trials=self.n_trials,
+                       metric=self.metric, direction=self.direction, k=self.l, seed=self.seed,
+                       max_epochs=self.max_epochs)
 
-        # we init the list that will contains the scores
-        scores = []
+    def create_model(self, best_hyper_params):
+        """
+        Method to create the Model
 
-        for i in range(self.k):
-            # we the get the train and the test datasets
-            train_set, valid_set, test_set = all_datasets[i]["train"], all_datasets[i]["valid"], all_datasets[i]["test"]
+        :param best_hyper_params: Python list that contains a set of hyper parameter used in the creation of the neural
+         network model
+        """
+        return self.model_generator(layers=best_hyper_params["layers"], dropout=best_hyper_params["dropout"],
+                                    activation=best_hyper_params["activation"])
 
-            # we create the tuner to perform the hyperparameters optimisation
-            tuner = NNTuner(model_generator=self.model_generator, datasets=all_datasets[i]["inner"],
-                            hyper_params=self.hyper_params, n_trials=self.n_trials,
-                            metric=self.metric, direction=self.direction, k=self.l, seed=self.seed,
-                            max_epochs=self.max_epochs)
+    def create_trainer(self, model, best_hyper_params):
+        """
+        Method to create a trainer object that will be used to train of our model
 
-            # we perform the hyper parameters tuning to get the best hyper parameters
-            best_hyper_params = tuner.tune()
+        :param model: The Neural Network model we want to train
+        :param best_hyper_params: Python list that contains a set of hyper parameter used in the training of the model
 
-            # we create our model with the best hyper parameters
-            model = self.model_generator(layers=best_hyper_params["layers"], dropout=best_hyper_params["dropout"],
-                                         activation=best_hyper_params["activation"])
-
-            # we create a trainer to train the model
-            trainer = NNTrainer(model, epochs=self.max_epochs,batch_size=best_hyper_params["batch_size"],
-                                lr=best_hyper_params["lr"], weight_decay=best_hyper_params["weight_decay"],
-                                seed=self.seed)
-
-            # we train our model with the best hyper parameters
-            trainer.fit(train_set=train_set, val_set=valid_set)
-
-            # we extract x_cont, x_cat and target from the validset
-            x_cont = test_set.X_cont
-            target = test_set.y
-            if test_set.X_cat is not None:
-                x_cat = test_set.X_cat
-            else:
-                x_cat = None
-
-            # we calculate the score with the help of the metric function
-            scores.append(self.metric(model(x_cont, x_cat).float(), target))
-
-        return sum(scores) / len(scores)
+        """
+        return NNTrainer(model, epochs=self.max_epochs, batch_size=best_hyper_params["batch_size"],
+                         lr=best_hyper_params["lr"], weight_decay=best_hyper_params["weight_decay"],
+                         seed=self.seed)
 
 
 class RFEvaluator(Evaluator):
     def __init__(self, model_generator, sampler, hyper_params, n_trials, metric, k, l, max_epochs=100,
                  direction="minimize", seed=None):
-
         """
         Class that will be responsible of the evaluation of the Random Forest models
 
@@ -119,45 +157,43 @@ class RFEvaluator(Evaluator):
         super().__init__(model_generator=model_generator, sampler=sampler, hyper_params=hyper_params, n_trials=n_trials,
                          metric=metric, k=k, l=l, direction=direction, seed=seed)
 
-    def nested_cross_valid(self):
+    def get_datasets(self, dataset_dictionary):
         """
-        Method to call when we want to perform a nested cross validation and evaluate the model
+        Method to extract the train, test, and valid sets
 
-        :return: the scores of the model after performing a nested cross validation
+        :param dataset_dictionary: Python dictionary that contains the three sets
+
+        """
+        return dataset_dictionary["train"], dataset_dictionary["test"], None
+
+    def create_tuner(self, datasets):
+        """
+        Method to create the Tuner object that will be used in the hyper parameters tuning
+
+        :param datasets: Python list that contains all the inner train, inner test, amd inner valid sets
+
+        """
+        return RFTuner(model_generator=self.model_generator, datasets=datasets,
+                       hyper_params=self.hyper_params, n_trials=self.n_trials,
+                       metric=self.metric, direction=self.direction, k=self.l, seed=self.seed)
+
+    def create_model(self, best_hyper_params):
+        """
+        Method to create the Model
+
+        :param best_hyper_params: Python list that contains a set of hyper parameter used in the creation of the Random
+         Forest model
+
+        """
+        return self.model_generator(n_estimators=best_hyper_params["n_estimators"])
+
+    def create_trainer(self, model, best_hyper_params):
+        """
+        Method to create a trainer object that will be used to train of our model
+
+        :param model: The Random Forest model we want to train
+        :param best_hyper_params: Python list that contains a set of hyper parameter used in the training of the model
+
         """
 
-        # we get all the train, test, inner train, qnd inner test sets with our sampler
-        all_datasets = self.sampler(k=self.k, l=self.l)
-
-        # we init the list that will contains the scores
-        scores = []
-
-        for i in range(self.k):
-            # we the get the train and the test datasets
-            train_set, test_set = all_datasets[i]["train"], all_datasets[i]["test"]
-
-            # we create the tuner to perform the hyperparameters optimisation
-            tuner = RFTuner(model_generator=self.model_generator, datasets=all_datasets[i]["inner"],
-                            hyper_params=self.hyper_params, n_trials=self.n_trials,
-                            metric=self.metric, direction=self.direction, k=self.l, seed=self.seed)
-
-            # we perform the hyper parameters tuning to get the best hyper parameters
-            best_hyper_params = tuner.tune()
-
-            # we create our model with the best hyper parameters
-            model = self.model_generator(n_estimators=best_hyper_params["n_estimators"])
-
-            # we create a trainer to train the model
-            trainer = RFTrainer(model)
-
-            # we train our model with the best hyper parameters
-            trainer.fit(train_set=train_set)
-
-            # we extract x_cont, x_cat and target from the validset
-            x_cont = test_set.X_cont
-            target = test_set.Y
-
-            # we calculate the score with the help of the metric function
-            scores.append(self.metric(trainer.predict(x_cont), target))
-
-        return sum(scores) / len(scores)
+        return RFTrainer(model)

408623737b4e87ad7b2f3042be5b11570e5affed
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-03-23 17:05:41 -0400

    Functions return types added

--- Utils/score_metrics.py
+++ Utils/score_metrics.py
@@ -9,7 +9,7 @@ from torch import sqrt, abs, tensor
 class RegressionMetrics:
 
     @staticmethod
-    def pearson(pred: tensor, targets: tensor):
+    def pearson(pred: tensor, targets: tensor) -> tensor:
         """
         Computes the pearson correlation coefficient between predictions and targets
         NOTE! : A strong correlation does not imply good accuracy
@@ -24,7 +24,7 @@ class RegressionMetrics:
         return p.dot(t) / (sqrt((p**2).sum())*sqrt((t**2).sum()))
 
     @staticmethod
-    def mean_absolute_error(pred: tensor, targets: tensor):
+    def mean_absolute_error(pred: tensor, targets: tensor) -> tensor:
         """
         Computes the mean absolute error between pred and targets
 
@@ -38,7 +38,7 @@ class RegressionMetrics:
 class ClassificationMetrics:
 
     @staticmethod
-    def accuracy(pred: tensor, targets: tensor):
+    def accuracy(pred: tensor, targets: tensor) -> tensor:
         """
         Returns the accuracy of predictions
 

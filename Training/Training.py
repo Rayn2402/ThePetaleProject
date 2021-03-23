6fef5f121191178e558b4b834a7eb22b07a4f655
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-03-22 21:01:53 -0400

    Seed management changed

--- Training/Training.py
+++ Training/Training.py
@@ -7,7 +7,7 @@ Files that contains class related to the Training of the models
 from .EarlyStopping import EarlyStopping
 from torch.nn import Module
 from torch.utils.data import DataLoader, Subset
-from torch import optim, manual_seed, cuda
+from torch import optim, manual_seed, cuda, tensor
 from torch import device as device_
 from tqdm import tqdm
 from optuna import TrialPruned
@@ -30,7 +30,7 @@ class Trainer:
         self.criterion = model.criterion_function
 
     def fit(self, train_set, val_set, batch_size, lr, weight_decay, epochs, early_stopping_activated=True,
-            patience=5, seed=None, device="cpu", trial=None, metric=None):
+            patience=5, device="cpu", trial=None, metric=None, seed=None):
         """
         Method that will fit the model to the given data
 
@@ -43,15 +43,15 @@ class Trainer:
         :param early_stopping_activated: Boolean indicating if we want to early stop
                                          the training when the validation loss stops decreasing
         :param patience: Int representing how long to wait after last time validation loss improved.
-        :param seed: The starting point in generating random numbers
         :param device: Device where we want to run our training, this parameter can take two values : "cpu" or "gpu"
         :param trial: Optuna Trial to report intermediate value
-        :param metric: a function that takes the output of the model and the target and returns the metric we want to
-        measure
+        :param metric: A function that takes the output of the model and the target and returns the metric we want to
+                       measure
+        :param seed: The starting point in generating random numbers
 
         :return: Two lists containing the training losses and the validation losses
         """
-
+        # Seed is left to None if fit his called by NNEvaluator
         if seed is not None:
             manual_seed(seed)
 
@@ -80,7 +80,12 @@ class Trainer:
         # we declare the variable which will hold the device we’re training on 
         device = device_("cuda" if cuda.is_available() and device == "gpu" else "cpu")
 
-        for epoch in tqdm(range(epochs)):
+        # We declare a tqdm loading bar
+        bar = tqdm(range(epochs), position=0, leave=True)
+        bar.set_description(f'Epoch 0')
+        bar.set_postfix_str(s=f"Loss : NaN")
+
+        for epoch in range(epochs):
 
             ###################
             # train the model #
@@ -111,8 +116,14 @@ class Trainer:
                 # Perform a single optimization step (parameter update)
                 optimizer.step()
 
+            # print(f"Epoch : {epoch} - loss {epoch_loss/len(train_loader)}")
+            mean_epoch_loss = epoch_loss / len(train_loader)
+            bar.set_description(f'Epoch {epoch}')
+            bar.set_postfix_str(s=f"Loss : {mean_epoch_loss}")
+            bar.update()
+
             # Record training loss
-            training_loss.append(epoch_loss / len(train_loader))
+            training_loss.append(mean_epoch_loss)
 
             ######################
             # validate the model #
@@ -138,8 +149,10 @@ class Trainer:
                 intermediate_score = metric(preds, y)
 
             if trial is not None:
+
                 # we report the score to optuna
                 trial.report(intermediate_score, step=epoch)
+
                 # we prune the trial if it should be pruned
                 if trial.should_prune():
                     raise TrialPruned()
@@ -151,7 +164,7 @@ class Trainer:
             if early_stopping.early_stop:
                 break
 
-        return training_loss, valid_loss
+        return tensor(training_loss), tensor(valid_loss)
 
     def cross_valid(self, datasets, batch_size, lr, weight_decay, epochs, metric, k=5, early_stopping_activated=True,
                     patience=5, trial=None, seed=None):
@@ -175,15 +188,20 @@ class Trainer:
 
         :return: returns the score after performing the k-fold cross validation
         """
+        # Seed is left to None if fit his called by NNTuner
+        if seed is not None:
+            manual_seed(seed)
 
         # we initialize an empty list to store the scores
         score = []
         for i in range(k):
+
             # we the get the train and the validation datasets of the step we are currently in
             train_set, valid_set, test_set = datasets[i]["train"], datasets[i]["valid"], datasets[i]["test"]
+
             # we train our model with this train and validation dataset
             self.fit(train_set=train_set, val_set=valid_set, batch_size=batch_size, lr=lr, weight_decay=weight_decay,
-                     epochs=epochs, early_stopping_activated=early_stopping_activated, patience=patience, seed=seed,
+                     epochs=epochs, early_stopping_activated=early_stopping_activated, patience=patience,
                      trial=trial, metric=metric)
 
             # we extract x_cont, x_cat and target from the subset valid_fold
@@ -253,5 +271,6 @@ def get_kfold_data(dataset, k, i):
         else:
             # we save the indexes of the items of the training set
             train_idx += idx
+
     # we return two subsets of the dataset, one representing the training set and one representing the validation set
     return Subset(dataset, train_idx), Subset(dataset, list(valid_idx))

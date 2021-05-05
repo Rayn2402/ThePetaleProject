e5bc224fe7148d0142a029018277bfb11a9a7aa1
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-05-05 15:12:04 -0400

    Cleaner changed to identify potential outliers instead of removing them

--- Data/Cleaning.py
+++ Data/Cleaning.py
@@ -1,7 +1,10 @@
 """
 This file stores the class DataCleaner used remove invalid rows and columns from learning dataframes
 """
+
 import pandas as pd
+from numpy import mean, cov, linalg, array, einsum, sqrt
+from scipy.stats import chi2
 from SQL.NewTablesScripts.constants import PARTICIPANT
 from SQL.DataManager.Helpers import retrieve_numerical
 from typing import List, Optional
@@ -16,41 +19,47 @@ class DataCleaner:
     and then remove outliers according to their numerical attributes.
     """
     # RECORDS CSTS
-    RP = "Removed Patients"
-    RC = "Removed Columns"
+    CP = "Critical Patients"
+    CC = "Critical Columns"
     CQD = "Column Quartile Details"
 
     # PRE-SAVED WARNING MESSAGES
-    ROW_TRESHOLD_WARNING = "Row threshold condition was not fulfilled"
-    COL_THRESHOLD_WARNING = "Column threshold condition was not fulfilled"
+    ROW_TRESHOLD_WARNING = "Row threshold condition not fulfilled"
+    COL_THRESHOLD_WARNING = "Column threshold condition not fulfilled"
 
     # OUTLIER LVLS
     LOW = "LOW"
     HIGH = "HIGH"
 
+    # MAHALANOBIS COLUMN ID
+    MAHALANOBIS = "Mahalanobis"
+
     def __init__(self, column_thresh: float, row_thresh: float,
-                 outlier_alpha: float, records_path: str):
+                 outlier_alpha: float, qchi2_mahalanobis_cutoff: float, records_path: str):
         """
         Class constructor
 
         :param column_thresh: percentage threshold (0 <= thresh <= 1)
         :param row_thresh: percentage threshold (0 <= thresh <= 1)
         :param outlier_alpha: constant multiplied by inter quartile range (IQR) to determine outliers
+        :param qchi2_mahalanobis_cutoff: Chi-squared quantile used to determine Mahalanobis cutoff value
         :param records_path: json file path to save results of cleaning
         """
         # Internal private fixed attributes
         self.__column_tresh = column_thresh
         self.__row_thresh = row_thresh
         self.__outlier_alpha = outlier_alpha
+        self.__qchi2 = qchi2_mahalanobis_cutoff
         self.__records_path = records_path
 
         # Private mutable attribute
-        self.__records = {self.RP: {},
-                          self.RC: [],
+        self.__records = {self.CP: {},
+                          self.CC: [],
                           "Column Threshold": self.__column_tresh,
                           "Row Threshold": self.__row_thresh,
                           "Outlier Alpha": self.__outlier_alpha,
-                          self.CQD: {}}
+                          self.CQD: {},
+                          self.MAHALANOBIS: {}}
 
     def __call__(self, df: pd.DataFrame, numerical_columns: Optional[List[str]] = None) -> pd.DataFrame:
         """
@@ -66,16 +75,17 @@ class DataCleaner:
             numerical_columns = list(numerical_df.columns.values)
 
         # We proceed to data cleaning
-        updated_df = self.__clean_dataframe(df)
+        updated_df = self.__identify_critical_rows_and_columns(df)
         updated_df = self.__refactor_dataframe(updated_df, numerical_columns)
-        updated_df = self.__remove_outliers(updated_df, numerical_columns)
+        self.__identify_univariate_outliers(updated_df, numerical_columns)
+        self.__identify_multivariate_outliers(updated_df, numerical_columns)
 
         # We save the records
         self.__save_records()
 
         return updated_df
 
-    def __clean_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
+    def __identify_critical_rows_and_columns(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Removes rows and columns with too many missing values
         """
@@ -89,24 +99,23 @@ class DataCleaner:
         # Records update
         for participant in df[PARTICIPANT].values:
             if participant not in updated_df[PARTICIPANT].values:
-                self.__records[self.RP][participant] = self.ROW_TRESHOLD_WARNING
+                self.__records[self.CP][participant] = {0: self.ROW_TRESHOLD_WARNING}
 
         for column in df.columns:
             if column not in updated_df.columns:
-                self.__records[self.RC][column] = self.COL_THRESHOLD_WARNING
+                self.__records[self.CC][column] = self.COL_THRESHOLD_WARNING
 
         return updated_df
 
-    def __remove_outliers(self, df: pd.DataFrame, numerical_columns: List[str]) -> pd.DataFrame:
+    def __identify_univariate_outliers(self, df: pd.DataFrame, numerical_columns: List[str]) -> None:
         """
-        Removes patients (rows) with numerical attribute value lower than
+        Identifies patients (rows) with numerical attribute value lower than
         Q1 - alpha*(Q3-Q1) or higher than Q3 + alpha*(Q3-Q1)
 
         :param df: pandas dataframe
         :param numerical_columns: list with names of numerical columns
         """
-        # For each column, computes quartiles and IQR = (Q3 - Q1) and remove outliers
-        updated_df = df
+        # For each column, computes quartiles and IQR = (Q3 - Q1) and identify outliers
         for c in numerical_columns:
 
             # Quartiles computation
@@ -115,18 +124,45 @@ class DataCleaner:
             iqr = round(q3 - q1, 4)
             self.__records[self.CQD][c] = {"Q1": q1, "Q2": q2, "Q3": q3, "IQR": iqr}
 
-            # Outliers removal (< Q1)
-            low_outlier = df.loc[df[c] < q1 - self.__outlier_alpha*iqr, [PARTICIPANT, c]]
-            updated_df = self.__update_outliers_records(df, low_outlier, c, self.LOW)
+            # Outliers identification (< Q1)
+            low_outliers = df.loc[df[c] < q1 - self.__outlier_alpha*iqr, [PARTICIPANT, c]]
+            self.__update_outliers_records(low_outliers, c, self.LOW)
 
-            # Outliers removal (> Q3)
-            high_outlier = df.loc[df[c] > q3 + self.__outlier_alpha*iqr, [PARTICIPANT, c]]
-            updated_df = self.__update_outliers_records(updated_df, high_outlier, c, self.HIGH)
+            # Outliers identification (> Q3)
+            high_outliers = df.loc[df[c] > q3 + self.__outlier_alpha*iqr, [PARTICIPANT, c]]
+            self.__update_outliers_records(high_outliers, c, self.HIGH)
 
-        return updated_df
+    def __identify_multivariate_outliers(self, df: pd.DataFrame, numerical_columns: List[str]) -> None:
+        """
+        Identifies patients with Mahalanobis distances abnormally high
+        (over the qchi2 quantile of Chi-squared distribution with D degrees of freedom)
+        where D is the number of numerical dimensions in our dataframe (after cleaning).
 
-    def __update_outliers_records(self, complete_df: pd.DataFrame, subset: pd.DataFrame,
-                                  column: str, lvl: str) -> pd.DataFrame:
+        :param df: pandas dataframe
+        :param numerical_columns: list with names of numerical columns
+        """
+        # We calculate Mahalanobis distances for all patients
+        temporary_df = df.loc[:, [PARTICIPANT]]
+        temporary_df[self.MAHALANOBIS] = self.__calculate_squared_mahalanobis_distances(df, numerical_columns)
+        print(temporary_df.columns)
+
+        # We calculate a cutoff values based on a Chi-squared distribution
+        cutoff = chi2.ppf(self.__qchi2, len(numerical_columns))
+
+        # We save distances to records
+        for i in range(temporary_df.shape[0]):
+
+            # We extract patient's id and value
+            patient, value = temporary_df.iloc[i, 0], round(temporary_df.iloc[i, 1], 4)
+            self.__records[self.MAHALANOBIS][patient] = value
+
+            # We had a warning message to the patient if its value is over the cutoff
+            if value > cutoff:
+                warning = self.__return_mahalanobis_warning(value)
+                self.__update_single_outlier_records(patient, warning)
+
+    def __update_outliers_records(self, subset: pd.DataFrame,
+                                  column: str, lvl: str) -> None:
         """
         Adds outliers data to records
 
@@ -137,10 +173,27 @@ class DataCleaner:
 
         """
         for i in range(subset.shape[0]):
-            self.__records[self.RP][subset.iloc[i, 0]] = \
-                self.__return_outlier_warning(column, subset.iloc[i, 1], lvl)
 
-        return complete_df.loc[~complete_df[PARTICIPANT].isin(list(subset[PARTICIPANT].values))]
+            # We extract patient's id and value
+            patient, value = subset.iloc[i, 0], subset.iloc[i, 1]
+
+            # We create the appropriate warning message
+            warning = self.__return_outlier_warning(column, value, lvl)
+
+            # We add the warning to the records
+            self.__update_single_outlier_records(patient, warning)
+
+    def __update_single_outlier_records(self, patient: str, warning: str):
+        """
+        Add warning message to a patient in the records
+
+        :param patient: str
+        :param warning: str, warning message
+        """
+        if patient in self.__records[self.CP].keys():
+            self.__records[self.CP][patient].update({len(self.__records[self.CP][patient].keys()): warning})
+        else:
+            self.__records[self.CP][patient] = {0: warning}
 
     def __save_records(self) -> None:
         """
@@ -149,6 +202,34 @@ class DataCleaner:
         # We save all the removed rows and columns collected in a json file
         with open(self.__records_path, "w") as file:
             dump(self.__records, file, indent=True)
+
+    @staticmethod
+    def __calculate_squared_mahalanobis_distances(df: pd.DataFrame, numerical_columns: List[str]) -> array:
+        """
+        Computes mahalanobis distances of all patients according to their numerical values
+
+        :param df: pandas dataframe
+        :param numerical_columns: list with names of numerical columns
+        :return: numpy array
+        """
+        # We fill NaN values with column means transform the data to numpy array
+        numerical_df = df[numerical_columns].fillna(df[numerical_columns].mean())
+        X = numerical_df.to_numpy().reshape((df.shape[0], -1))
+
+        # We get the center point, the vector composed of the means of each variable
+        centroid = mean(X, axis=0).reshape(1, numerical_df.shape[1])
+
+        # We get the inverse of the covariance matrix
+        covariance_inv = linalg.matrix_power(cov(X, rowvar=False), -1)
+
+        # We compute the first matrix multiplication
+        A = (X - centroid)
+        B = A.dot(covariance_inv)
+
+        # We finish calculating distances all at once with einsum
+        squared_distances = einsum('ij,ij->i', B, A)
+
+        return squared_distances
 
     @staticmethod
     def __refactor_dataframe(df: pd.DataFrame, numerical_columns: List[str]) -> pd.DataFrame:
@@ -175,7 +256,7 @@ class DataCleaner:
                     Q3 + alpha*(Q3-Q1)
         :return: str
         """
-        return f"Patient was considered as an outlier" \
+        return f"Patient targeted as an outlier" \
                f" due to a {lvl} '{numerical_attribute}' value of {value}"
 
     @staticmethod
@@ -186,4 +267,4 @@ class DataCleaner:
         :param value: Patient's Mahalanobis distance
         :return: str
         """
-        return f"Patient was considered as an outlier due to its Mahalanobis distance of {value}"
+        return f"Patient targeted as an outlier due to its Mahalanobis distance of {value}"

3fb47153914e6a43ddad826575a1f7d1a62fa8d5
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-05-10 15:58:13 -0400

    Type hints added

--- Data/Transforms.py
+++ Data/Transforms.py
@@ -4,6 +4,7 @@ Author : Nicolas Raymond
 This file contains all transformations related to preprocessing treatment
 """
 
+from typing import Optional
 import pandas as pd
 
 
@@ -12,14 +13,15 @@ class ContinuousTransform:
     Class of transformation that can be applied to continuous data
     """
     @staticmethod
-    def to_float(df):
+    def to_float(df: pd.DataFrame) -> pd.DataFrame:
         """
         Changes type of pandas columns to float
         """
         return df.astype('float')
 
     @staticmethod
-    def normalize(df, mean=None, std=None):
+    def normalize(df: pd.DataFrame, mean: Optional[pd.Series] = None,
+                  std: Optional[pd.Series] = None) -> pd.DataFrame:
         """
         Applies normalization to columns of a pandas dataframe
         """
@@ -29,7 +31,7 @@ class ContinuousTransform:
             return (df-df.mean())/df.std()
 
     @staticmethod
-    def fill_missing(df, mean=None):
+    def fill_missing(df: pd.DataFrame, mean: Optional[pd.Series] = None) -> pd.DataFrame:
         """
         Fills missing values of continuous data columns with mean
         """
@@ -45,21 +47,21 @@ class CategoricalTransform:
     """
 
     @staticmethod
-    def to_category(df):
+    def to_category(df: pd.DataFrame) -> pd.DataFrame:
         """
         Changes type of pandas column to category
         """
         return df.astype('category')
 
     @staticmethod
-    def one_hot_encode(df):
+    def one_hot_encode(df: pd.DataFrame) -> pd.DataFrame:
         """
         One hot encodes all columns of the dataframe
         """
         return pd.get_dummies(df)
 
     @staticmethod
-    def ordinal_encode(df):
+    def ordinal_encode(df: pd.DataFrame) -> pd.DataFrame:
         """
         Applies ordinal encoding to all columns of the dataframe
         """
@@ -69,7 +71,7 @@ class CategoricalTransform:
         return df
 
     @staticmethod
-    def fill_missing(df, mode=None):
+    def fill_missing(df: pd.DataFrame, mode: Optional[pd.Series] = None) -> pd.DataFrame:
         """
         Fills missing values of continuous data columns with mode
         """

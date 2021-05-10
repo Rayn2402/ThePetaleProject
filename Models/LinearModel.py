65adfe39ed091bd8a06e6a9f6da90daaa94c2fb5
Mehdi Mitiche <gm_mitiche@esi.dz>
2021-05-10 16:48:31 -0400

    regularization add to linear model

--- Models/LinearModel.py
+++ Models/LinearModel.py
@@ -6,23 +6,35 @@ LinearRegressor which is the class that will represent the analytical solution a
 GDLinearRegressor which is the class that will represent the model of the linear regression with gradient descent
 """
 
-from torch import randn, matmul, cat, inverse, transpose
+from torch import randn, matmul, cat, inverse, transpose, eye
 from torch.nn import Module, ModuleList, Embedding, Linear, MSELoss
 
 
 class LinearRegressor:
 
-    def __init__(self, input_size):
+    def __init__(self, input_size, regularization=False, regularization_value=None):
         """
         Creates a model that give us the analytical solution of the  linear regression 
         :param input_size: the number of features we have
+        :param regularization: (boolean) Specify if we want to use regularization (True) or not (False)
+        :param regularization_value: (number) The lambda value used in the regularization
         """
+
+        # We check if the regularization_value is specified if the regularization is activated
+        assert regularization is False or regularization_value is not None, "regularization value must be specified"
+
         # we initialize the weights with random numbers
         self.W = randn(input_size, 1)
+        self.regularization = regularization
+        self.regularization_value = regularization_value
 
     def train(self, x, y):
         # we find weights using the analytical solution formula of the linear regression
-        self.W = matmul(matmul(inverse(matmul(transpose(x, 0, 1), x)), transpose(x, 0, 1)), y)
+        if self.regularization is False:
+            self.W = matmul(matmul(inverse(matmul(transpose(x, 0, 1), x)), transpose(x, 0, 1)), y)
+        else:
+            self.W = matmul(matmul(inverse((matmul(transpose(x, 0, 1), x) +
+                                            eye(x.shape[1]) * self.regularization_value)), transpose(x, 0, 1)), y)
 
     def predict(self, x):
         """

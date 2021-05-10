4a241d831272db8e4005b6809bb9915eaffd0696
Mehdi Mitiche <gm_mitiche@esi.dz>
2021-05-10 18:02:25 -0400

    linear regression experience updated

--- Experiments/WarmUp/LinearRegressionExp.py
+++ Experiments/WarmUp/LinearRegressionExp.py
@@ -8,57 +8,65 @@ from Utils.score_metrics import RegressionMetrics
 from Recording.Recorder import RFRecorder, get_evaluation_recap, compare_prediction_recordings
 from os.path import join
 
-manager = PetaleDataManager("mitm2902")
 
-EVALUATION_NAME = "LinearRegression"
-RECORDING_PATH = join("..", "..")
+def execute_linear_regression_experience(k, regularization=False, lambda_values=[None]):
+    manager = PetaleDataManager("mitm2902")
 
-# We create the warmup sampler to get the data
-warmup_sampler = get_warmup_sampler(dm=manager)
-data = warmup_sampler(k=10, valid_size=0, add_biases=True)
+    evaluation_name = f"LinearRegression_{'r' if regularization is True else 'nr'}_k{k}"
+    RECORDING_PATH = join("..", "..")
 
-linear_regression_scores = []
+    # We create the warmup sampler to get the data
+    warmup_sampler = get_warmup_sampler(dm=manager, outliers_ids=["P140", "P108"])
+    data = warmup_sampler(k=k, valid_size=0, add_biases=True)
 
-features = ["B", "WEIGHT", "TDM6_HR_END", "TDM6_DIST", "DT", "AGE", "MVLPA"]
+    linear_regression_scores = []
 
-for i in range(10):
-    # We create the linear regressor
-    linearRegressor = LinearRegressor(input_size=7)
+    features = ["B", "WEIGHT", "TDM6_HR_END", "TDM6_DIST", "DT", "AGE", "MVLPA"]
 
-    # We create the recorder
-    recorder = RFRecorder(evaluation_name=EVALUATION_NAME, index=i, recordings_path=RECORDING_PATH)
+    for value in lambda_values:
+        evaluation_name = f"{evaluation_name}_{value if value is not None else ''}"
+        for i in range(k):
+            # We create the linear regressor
+            linear_regressor = LinearRegressor(input_size=7, regularization=regularization, lambda_value=value)
 
-    recorder.record_data_info("train_set", len(data[i]["train"]))
-    recorder.record_data_info("test_set", len(data[i]["test"]))
+            # We create the recorder
+            recorder = RFRecorder(evaluation_name=evaluation_name, index=i, recordings_path=RECORDING_PATH)
 
-    # We train the linear regressor
-    linearRegressor.train(x=data[i]["train"].X_cont, y=data[i]["train"].y)
+            recorder.record_data_info("train_set", len(data[i]["train"]))
+            recorder.record_data_info("test_set", len(data[i]["test"]))
 
-    for j, feature in enumerate(features):
-        recorder.record_coefficient(name=feature, value=linearRegressor.W[j].item())
+            # We train the linear regressor
+            linear_regressor.train(x=data[i]["train"].X_cont, y=data[i]["train"].y)
 
-    # We make our predictions
-    linear_regression_pred = linearRegressor.predict(x=data[i]["test"].X_cont)
+            for j, feature in enumerate(features):
+                recorder.record_coefficient(name=feature, value=linear_regressor.W[j].item())
 
-    # We save the predictions
-    recorder.record_predictions(ids=data[i]["test"].IDs, predictions=linear_regression_pred.numpy().astype("float64"),
-                                target=data[i]["test"].y.numpy().astype("float64"))
+            # We make our predictions
+            linear_regression_pred = linear_regressor.predict(x=data[i]["test"].X_cont)
 
-    # We calculate the score
-    score = RegressionMetrics.mean_absolute_error(linear_regression_pred, data[i]["test"].y)
+            # We save the predictions
+            recorder.record_predictions(ids=data[i]["test"].IDs,
+                                        predictions=linear_regression_pred.numpy().astype("float64"),
+                                        target=data[i]["test"].y.numpy().astype("float64"))
 
-    # We save the score
-    recorder.record_scores(score=score, metric="mean_absolute_error")
+            # We calculate the score
+            score = RegressionMetrics.mean_absolute_error(linear_regression_pred, data[i]["test"].y)
 
-    # We save the metric score
-    linear_regression_scores.append(score)
+            # We save the score
+            recorder.record_scores(score=score, metric="mean_absolute_error")
 
-    # We generate the file containing the saved data
-    recorder.generate_file()
+            # We save the metric score
+            linear_regression_scores.append(score)
 
-    compare_prediction_recordings(evaluations=[EVALUATION_NAME], split_index=i, recording_path=RECORDING_PATH)
+            # We generate the file containing the saved data
+            recorder.generate_file()
 
-# We generate the evaluation recap
-get_evaluation_recap(evaluation_name=EVALUATION_NAME, recordings_path=RECORDING_PATH)
+            compare_prediction_recordings(evaluations=[evaluation_name], split_index=i, recording_path=RECORDING_PATH)
 
-print(linear_regression_scores)
+        # We generate the evaluation recap
+        get_evaluation_recap(evaluation_name=evaluation_name, recordings_path=RECORDING_PATH)
+
+        print(linear_regression_scores)
+
+
+execute_linear_regression_experience(k=10, regularization=False)

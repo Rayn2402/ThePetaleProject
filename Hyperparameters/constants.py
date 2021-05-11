0d93d4f142b9b7fa18dbc893865b41491a3ab03d
mehdiMitiche <gm_mitiche@esi.dz>
2021-05-11 18:11:07 -0400

    hyperparameters constants added

---
+++ Hyperparameters/constants.py
@@ -0,0 +1,30 @@
+"""
+This file is used to store useful constants related to hyperparameters name
+"""
+
+# HYPERPARAMETERS IMPORTANCE SEED
+HYPER_PARAMS_SEED = 2021
+
+# NN HYPERPARAMETERS
+LR = "lr"
+BATCH_SIZE = "batch_size"
+N_LAYERS = "n_layers"
+N_UNITS = "n_units"
+DROPOUT = "dropout"
+ACTIVATION = "activation"
+WEIGHT_DECAY = "weight_decay"
+LAYERS = "layers"
+
+# RANGE
+MIN = "min"
+MAX = "max"
+VALUES = "values"
+VALUE = "value"
+
+# RF HYPERPARAMETERS
+N_ESTIMATORS = "n_estimators"
+MAX_DEPTH = "max_depth"
+MAX_FEATURES = "max_features"
+MAX_SAMPLES = "max_samples"
+
+

fc2044a14e39727aaa0ba1ccf80f8637ed52a28d
mehdiMitiche <gm_mitiche@esi.dz>
2021-05-13 15:17:16 -0400

    small improvments added

--- Experiments/WarmUp/NeuralNetwork.py
+++ Experiments/WarmUp/NeuralNetwork.py
@@ -24,6 +24,8 @@ def execute_neural_network_experiment(k: int, l: int, n_trials: int, hp_files_id
     :param hp_files_ids: List of ids of the hyperparameters file to test
     """
 
+    assert len(hp_files_ids) > 0, "At least one hyperparameter id must be specified"
+
     # We create the Petale Data Manager
     manager = PetaleDataManager("mitm2902")
 
@@ -43,20 +45,25 @@ def execute_neural_network_experiment(k: int, l: int, n_trials: int, hp_files_id
     generator = NNModelGenerator(NNRegressor, num_cont_col=data[0]["train"].X_cont.shape[1])
 
     for hp_id in hp_files_ids:
+        # We set the hyperparameter file id
+        hp_id = str(hp_id)
+        hp_id = f"{(3-len(hp_id))*'0'}{hp_id}"
+
+        #We set the name of the evaluation
         evaluation_name = f"{evaluation_name}_hp{hp_id}"
 
         # We gather the hyperparameter to be optimized
-        with open(join(RECORDING_PATH, "Hyperparameters", f"TEST_1_0{hp_id}.json"), "r") as read_file:
+        with open(join(RECORDING_PATH, "Hyperparameters", f"TEST_1_{hp_id}.json"), "r") as read_file:
             HYPER_PARAMS = load(read_file)
 
         # We create the Evaluator Object
         evaluator = NNEvaluator(model_generator=generator, sampler=warmup_sampler, k=k, l=l,
-                                  hyper_params=HYPER_PARAMS,
-                                  optimization_metric=RegressionMetrics.mean_absolute_error,
-                                  evaluation_metrics=evaluation_metrics, n_trials=n_trials, seed=2019,
-                                  evaluation_name=evaluation_name, early_stopping_activated=True, max_epochs=500,
-                                  get_hyperparameters_importance=True, get_optimization_history=True,
-                                  get_parallel_coordinate=True, recordings_path=RECORDING_PATH)
+                                hyper_params=HYPER_PARAMS,
+                                optimization_metric=RegressionMetrics.mean_absolute_error,
+                                evaluation_metrics=evaluation_metrics, n_trials=n_trials, seed=2019,
+                                evaluation_name=evaluation_name, early_stopping_activated=True, max_epochs=500,
+                                get_hyperparameters_importance=True, get_optimization_history=True,
+                                get_parallel_coordinate=True, recordings_path=RECORDING_PATH)
 
         # We perform the nested cross validation
         evaluator.nested_cross_valid()

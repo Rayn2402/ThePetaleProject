d0884d70c151cca9fce8099e9a609b9346385bd1
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-06-07 10:42:45 -0400

    Distribution sanity check adapted

--- SanityChecks/SamplingDistribution.py
+++ SanityChecks/SamplingDistribution.py
@@ -3,45 +3,60 @@ This file is used to evaluate if the stratified sampling is correctly
 done by the RandomStratifiedSampler class
 """
 
-from Data.Datasets import PetaleNNDataset
-from Data.Sampling import RandomStratifiedSampler
+from Data.Datasets import PetaleRFDataset
+from Data.Sampling import get_learning_one_data, extract_masks, generate_multitask_labels
+from os.path import join, dirname
 from SQL.constants import *
 from SQL.DataManagement.Utils import PetaleDataManager
 from Utils.visualization import visualize_class_distribution
 
 
 DATASET_TYPES = ['train', 'valid', 'test']
-LABELS_DICT = {0: "No", 1: "Yes"}
 
 
 if __name__ == '__main__':
 
-    # STRATIFIED SAMPLING TEST WITH LEARNING 01
+    # STRATIFIED SAMPLING CHECK WITH LEARNING 01
 
     print(f"\nStratified sampling test...\n")
 
     # Initialization of DataManager and sampler
     manager = PetaleDataManager("rayn2402")
-    sampler = RandomStratifiedSampler(1, 1)
 
-    # We save continuous columns and categorical columns
-    cont_cols = [AGE_AT_DIAGNOSIS, DT, DOX]
-    cat_cols = [SEX, RADIOTHERAPY_DOSE, DEX, BIRTH_AGE, BIRTH_WEIGHT]
+    # Extraction of the table
+    df, cont_cols, cat_cols = get_learning_one_data(manager, baselines=True,
+                                                    complications=[CARDIOMETABOLIC_COMPLICATIONS,
+                                                                   BONE_COMPLICATIONS,
+                                                                   NEUROCOGNITIVE_COMPLICATIONS,
+                                                                   COMPLICATIONS])
+    # Extraction of masks
+    masks = extract_masks(join(dirname(dirname(__file__)), "Masks", "L1_masks.json"), k=1, l=0)
 
-    # Creation of dataset
-    df = manager.get_table(LEARNING_1)
-    datasets = PetaleNNDataset(df, CARDIOMETABOLIC_COMPLICATIONS, cont_cols, cat_cols)
-
-    # Creation of masks
-    masks = sampler(datasets.y)
+    # Creation of a dataset
+    dataset = PetaleRFDataset(df, COMPLICATIONS, cont_cols, cat_cols)
 
     # Visualization of distribution
+    labels_dict = {0: "No", 1: "Yes"}
     for k, v in masks.items():
-        datasets.update_masks(train_mask=v["train"], valid_mask=v["valid"], test_mask=v["test"])
+        dataset.update_masks(train_mask=v["train"], valid_mask=v["valid"], test_mask=v["test"])
         if v != 'inner':
             for i in DATASET_TYPES:
-                visualize_class_distribution(datasets.y[v[i]], LABELS_DICT, title=f"{i}-{k}")
+                visualize_class_distribution(dataset.y[v[i]], labels_dict, title=f"{i}-{k}")
             for inner_k, inner_v in v['inner'].items():
                 for j in DATASET_TYPES:
-                    visualize_class_distribution(datasets.y[inner_v[j]], LABELS_DICT, title=f"Inner {j}-{inner_k}")
+                    visualize_class_distribution(dataset.y[inner_v[j]], labels_dict, title=f"Inner {j}-{inner_k}")
+
+    # Creation of multitask labels
+    labels, labels_dict = generate_multitask_labels(df, [CARDIOMETABOLIC_COMPLICATIONS,
+                                                         BONE_COMPLICATIONS,
+                                                         NEUROCOGNITIVE_COMPLICATIONS])
+
+    # Visualization of distribution for multitask labels
+    for k, v in masks.items():
+        if v != 'inner':
+            for i in DATASET_TYPES:
+                visualize_class_distribution(labels[v[i]], labels_dict, title=f"{i}-{k}")
+            for inner_k, inner_v in v['inner'].items():
+                for j in DATASET_TYPES:
+                    visualize_class_distribution(labels[inner_v[j]], labels_dict, title=f"Inner {j}-{inner_k}")
 

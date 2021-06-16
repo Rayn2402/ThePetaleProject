894d76ae0dbe1f6c5f8859063aa1dc5145988d7f
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-06-16 13:33:42 -0400

    Enum parent removed

--- settings/paths.py
+++ settings/paths.py
@@ -4,24 +4,24 @@ Authors : Nicolas Raymond
 Files that contains Paths enumerations
 
 """
-import os
-from enum import Enum
+from os.path import dirname, join
 
 
-class Paths(Enum):
+class Paths:
     """
     Paths of important folder and files
     """
-    PROJECT_DIR: str = os.path.dirname(os.path.dirname(__file__))
-    CSV_FILES: str = os.path.join(PROJECT_DIR, "csv")
-    HYPERPARAMETERS: str = os.path.join(PROJECT_DIR, "hps")
-    MASKS: str = os.path.join(PROJECT_DIR, "masks")
-    RECORDS: str = os.path.join(PROJECT_DIR, "records")
-    CLEANING_RECORDS: str = os.path.join(RECORDS, "cleaning")
-    DESC_RECORDS: str = os.path.join(RECORDS, "descriptive_analyses")
-    DESC_CHARTS: str = os.path.join(DESC_RECORDS, "charts")
-    DESC_STATS: str = os.path.join(DESC_RECORDS, "stats")
-    EXPERIMENTS_RECORDS: str = os.path.join(RECORDS, "experiments")
-    EXPERIMENTS_SCRIPTS: str = os.path.join(PROJECT_DIR, "experiments")
-    WARMUP_EXPERIMENTS_SCRIPTS: str = os.path.join(EXPERIMENTS_SCRIPTS, "warm_up")
+    PROJECT_DIR: str = dirname(dirname(__file__))
+    CSV_FILES: str = join(PROJECT_DIR, "csv")
+    HYPERPARAMETERS: str = join(PROJECT_DIR, "hps")
+    MASKS: str = join(PROJECT_DIR, "masks")
+    RECORDS: str = join(PROJECT_DIR, "records")
+    CLEANING_RECORDS: str = join(RECORDS, "cleaning")
+    DESC_RECORDS: str = join(RECORDS, "descriptive_analyses")
+    DESC_CHARTS: str = join(DESC_RECORDS, "charts")
+    DESC_STATS: str = join(DESC_RECORDS, "stats")
+    EXPERIMENTS_RECORDS: str = join(RECORDS, "experiments")
+    EXPERIMENTS_SCRIPTS: str = join(PROJECT_DIR, "experiments")
+    WARMUP_EXPERIMENTS_SCRIPTS: str = join(EXPERIMENTS_SCRIPTS, "warm_up")
+    SANITY_CHECKS: str = join(PROJECT_DIR, "sanity_checks")
 

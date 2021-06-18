2533a2753f3f9c56f3135fa0b78b138a65d5830d
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-06-18 09:56:46 -0400

    GDlinear removed since we can built it directly from NNregressor

--- src/models/linear_models.py
+++ src/models/linear_models.py
@@ -10,7 +10,7 @@ from torch import randn, matmul, cat, inverse, transpose, eye, tensor
 from torch.nn import Module, ModuleList, Embedding, Linear, MSELoss
 
 
-class LinearRegressor:
+class LinearRegression:
     """
     Linear regression using the analytical solution with MSE loss
     """
@@ -73,58 +73,3 @@ class LinearRegressor:
 
         """
         return matmul(x, self.__w)
-
-
-class GDLinearRegressor(Module):
-    def __init__(self, num_cont_col, cat_sizes=None):
-        """
-        Creates a model that perform a linear regression with gradient descent, entity embedding
-        is performed on the data if cat_sizes is not null
-
-        :param cat_sizes: list of integer representing the size of each categorical column
-        :param num_cont_col: the number of continuous columns we have
-        """
-        super(GDLinearRegressor, self).__init__()
-        if cat_sizes is not None:
-
-            # we generate the embedding sizes ( this part will be optimized )
-            embedding_sizes = [(cat_size, min(50, (cat_size + 1) // 2)) for cat_size in cat_sizes]
-
-            # we create the Embeddings layers
-            self.embedding_layers = ModuleList([Embedding(num_embedding, embedding_dim) for
-                                                num_embedding, embedding_dim in embedding_sizes])
-
-            # we get the number of our categorical data after the embedding ( we sum the embeddings dims)
-            num_cat_col = sum((embedding_dim for num_embedding, embedding_dim in embedding_sizes))
-
-            # the number of entries to our linear layer
-            input_size = num_cat_col + num_cont_col
-        else:
-            # the number of entries to our linear layer
-            input_size = num_cont_col
-
-        # we define our linear layer
-        self.linear = Linear(input_size, 1)
-        # we define the criterion for that model
-        self.criterion = MSELoss()
-
-    def forward(self, x_cont, x_cat=None):
-
-        embeddings = []
-        if x_cat is not None:
-
-            # we perform the entity embedding
-            for i, e in enumerate(self.embedding_layers):
-                embeddings.append(e(x_cat[:, i]))
-
-            # we concatenate all the embeddings
-            x = cat(embeddings, 1)
-
-            # we concatenate categorical and continuous data after performing the  entity embedding
-            x = cat([x, x_cont], 1)
-        else:
-            x = x_cont
-        return self.linear(x)
-
-    def loss(self, x_cont, x_cat, target):
-        return ((self(x_cont.float(), x_cat).squeeze() - target) ** 2).mean().item()

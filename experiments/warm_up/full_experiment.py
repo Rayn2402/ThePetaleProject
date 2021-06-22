ea16a6bbe317ea9b516aa2d52fc0d2a571b796f3
mehdiMitiche <gm_mitiche@esi.dz>
2021-06-22 11:17:58 -0400

    mvlpa now always present in the data to evaluate our models

--- experiments/warm_up/full_experiment.py
+++ experiments/warm_up/full_experiment.py
@@ -13,15 +13,14 @@ from settings.paths import Paths
 FILE_NN = str(join(Paths.WARMUP_EXPERIMENTS_SCRIPTS, "neural_network.py"))
 FILE_OE = str(join(Paths.WARMUP_EXPERIMENTS_SCRIPTS, "original_equation.py"))
 SEEDS = map(str, range(100, 106))
-COMMANDS_NN_1 = ['python3', FILE_NN, '-k', '10', '-l', '20', '-n', '1000', '-s', *SEEDS]
-COMMANDS_NN_2 = COMMANDS_NN_1 + ['-m']
+COMMANDS_NN_1 = ['python3', FILE_NN, '-k', '10', '-l', '20', '-n', '1000', '-s', *SEEDS, "-m"]
 SEEDS = map(str, range(100, 106))
 COMMANDS_OE = ['python3', FILE_OE, '-k', '10', '-s', *SEEDS]
 
 
 if __name__ == '__main__':
 
-    for cmd in [COMMANDS_NN_1, COMMANDS_NN_2, COMMANDS_OE]:
+    for cmd in [COMMANDS_NN_1, COMMANDS_OE]:
         # We run experiments
         start = time.time()
         p = sp.Popen(cmd)

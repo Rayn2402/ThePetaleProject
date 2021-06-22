151e8de4c1fad2e9b0204dd13451f140e43fba14
mehdiMitiche <gm_mitiche@esi.dz>
2021-06-22 10:15:41 -0400

    experiments files added

---
+++ experiments/warm_up/full_experiment.py
@@ -0,0 +1,30 @@
+"""
+This file consists of all the experiments made on the warmup dataset
+"""
+from os.path import dirname, realpath, join
+import sys
+
+sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
+import time
+import subprocess as sp
+from settings.paths import Paths
+
+
+FILE_NN = str(join(Paths.WARMUP_EXPERIMENTS_SCRIPTS, "neural_network.py"))
+FILE_OE = str(join(Paths.WARMUP_EXPERIMENTS_SCRIPTS, "original_equation.py"))
+SEEDS = map(str, range(100, 106))
+COMMANDS_NN_1 = ['python3', FILE_NN, '-k', '10', '-l', '20', '-n', '1000', '-s', *SEEDS]
+COMMANDS_NN_2 = COMMANDS_NN_1 + ['-m']
+SEEDS = map(str, range(100, 106))
+COMMANDS_OE = ['python3', FILE_OE, '-k', '10', '-s', *SEEDS]
+
+
+if __name__ == '__main__':
+
+    for cmd in [COMMANDS_NN_1, COMMANDS_NN_2, COMMANDS_OE]:
+        # We run experiments
+        start = time.time()
+        p = sp.Popen(cmd)
+        p.wait()
+
+    print("Time Taken (minutes): ", round((time.time() - start) / 60, 2))

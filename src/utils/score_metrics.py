be29a321fda86a2ea3459be6c2cde2eff8d09336
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-07-14 16:03:43 -0400

    score metrics updated

--- src/utils/score_metrics.py
+++ src/utils/score_metrics.py
@@ -4,8 +4,9 @@ Author : Nicolas Raymond
 This file contains metric used to measure models' performances
 """
 from abc import ABC, abstractmethod
-from torch import sqrt, abs, tensor, argmax, zeros, ones, eye, mean, prod, sum, pow
-from torch.nn.functional import nll_loss
+from torch import sqrt, abs, tensor, zeros, mean, prod, sum, pow
+from typing import Callable
+
 
 MAXIMIZE = "maximize"
 MINIMIZE = "minimize"
@@ -76,8 +77,8 @@ class Pearson(Metric):
         """
         Computes the pearson correlation coefficient between predictions and targets
         Args:
-            pred: (N,) tensor
-            targets: (N,) tensor
+            pred: (N,) tensor with predicted labels
+            targets: (N,) tensor with ground truth
 
         Returns: float
 
@@ -116,8 +117,8 @@ class AbsoluteError(Metric):
         Compute the absolute error between predictions and targets
 
         Args:
-            pred: (N,) tensor
-            targets: (N,) tensor
+            pred: (N,) tensor with predicted labels
+            targets: (N,) tensor with ground truth
 
         Returns: float
 
@@ -143,190 +144,100 @@ class RootMeanSquaredError(Metric):
         Compute the root mean-squared error between predictions and targets
 
         Args:
-            pred: (N,) tensor
-            targets: (N,) tensor
+            pred: (N,) tensor with predicted labels
+            targets: (N,) tensor with ground truth
 
         Returns: float
         """
         return round((mean((pred - targets)**2).item())**(1/2), self.n_digits)
 
 
-# class Accuracy(Metric):
-#     """
-#     Callable class that computes the accuracy
-#     """
-#     def __init__(self):
-#         super().__init__(MAXIMIZE, CLASSIFICATION)
-#
-#     def __call__(self, pred: tensor, targets: tensor) -> float:
-#         """
-#         Returns the accuracy of predictions
-#
-#         Args:
-#             pred: (N,C) tensor with log probabilities
-#             targets: (N,) tensor
-#
-#         Returns: float
-#
-#         """
-#         return (argmax(pred, dim=1).float() == targets.long()).float().mean().item()
-#
-#
-# class CrossEntropyLoss(Metric):
-#     """
-#     Callable class that computes the cross entropy loss
-#     """
-#     def __init__(self, reduction: str = "mean"):
-#         """
-#         Sets the protected reduction method
-#         Args:
-#             reduction: "mean" for mean cross entropy and "sum" for the sum of the cross entropy losses
-#         """
-#         super().__init__(MINIMIZE, CLASSIFICATION)
-#         self._reduction = reduction
-#
-#     def __call__(self, pred: tensor, targets: tensor) -> float:
-#         """
-#         Returns the cross entropy related to the predictions
-#
-#         Args:
-#             pred: (N,C) tensor with log probabilities
-#             targets: (N,) tensor
-#
-#         Returns: float
-#         """
-#         return nll_loss(pred, targets.long(), reduction=self._reduction).item()
-#
-#
-# class AccuracyCrossEntropyRatio(Metric):
-#     """
-#     Callable class that computes the ratio accuracy/cross_entropy
-#     """
-#
-#     def __init__(self, reduction: str = "mean"):
-#         """
-#         Sets the protected callable attributes accuracy and cross entropy
-#
-#         Args:
-#             reduction: "mean" for mean cross entropy and "sum" for the sum of the cross entropy losses
-#         """
-#         super().__init__(MAXIMIZE, CLASSIFICATION)
-#         self._accuracy = Accuracy()
-#         self._cross_entropy = CrossEntropyLoss(reduction=reduction)
-#
-#     def __call__(self, pred: tensor, targets: tensor) -> float:
-#         """
-#         Returns the ratio accuracy/cross-entropy related to the predictions
-#
-#         Args:
-#             pred: (N,C) tensor with log probabilities
-#             targets: (N,) tensor
-#
-#         Returns: float
-#
-#         """
-#         return self._accuracy(pred, targets)/self._cross_entropy(pred, targets)
-#
-#
-# class Sensitivity(Metric):
-#     """
-#     Callable class that compute classes' sensitivity using confusion matrix
-#     """
-#     def __init__(self, nb_classes: int, reduction: str = "mean"):
-#         """
-#         Sets the protected reduction method
-#
-#         Args:
-#             nb_classes: number of class in the classification problem
-#             reduction: "mean" for mean classes' sensitivity or "geometric_mean"
-#                        for geometric mean of classes' sensitivity
-#         """
-#         assert reduction in [MEAN, GEO_MEAN], f"Reduction must be in {[MEAN, GEO_MEAN]}"
-#         super().__init__(MAXIMIZE, CLASSIFICATION)
-#
-#         # We set the number of classes to build the confusions matrix
-#         self.nb_classes = nb_classes
-#
-#         if reduction == "mean":
-#             self._reduction = mean
-#         else:
-#             self._reduction = lambda x: pow(prod(x), exponent=x.shape[0])
-#
-#     def __call__(self, pred: tensor, targets: tensor) -> float:
-#         """
-#         Returns the classes' sensitivity
-#
-#         Args:
-#             pred: (N,C) tensor with log probabilities
-#             targets: (N,) tensor
-#
-#         Returns: float
-#         """
-#         # We get confusion matrix
-#         conf_mat = self.get_confusion_matrix(pred, targets)
-#
-#         # We first get true positives
-#         tp = conf_mat.diag()
-#
-#         # We then get false negatives (row sums of items of diagonal)
-#         fn = (conf_mat * (ones(conf_mat.shape)-eye(conf_mat.shape[0]))).sum(axis=1)
-#
-#         # We compute class sensitivity
-#         sensitivity = tp / (tp+fn)
-#
-#         return self._reduction(sensitivity).item()
-#
-#     def get_confusion_matrix(self, pred: tensor, targets: tensor) -> tensor:
-#         """
-#         Returns the confusion matrix
-#
-#         Args:
-#             pred: (N,C) tensor with log probabilities
-#             targets: (N,) tensor
-#
-#         Returns: (C,C) tensor
-#
-#         """
-#
-#         # We extract the class prediction and convert targets to long type
-#         pred = argmax(pred, 1)
-#         targets = targets.long()
-#
-#         # We initialize an empty confusion matrix
-#         conf_matrix = zeros(self.nb_classes, self.nb_classes)
-#
-#         # We fill the confusion matrix
-#         for t, p in zip(targets, pred):
-#             conf_matrix[t, p] += 1
-#
-#         return conf_matrix
-#
-#
-# class SensitivityCrossEntropyRatio(Metric):
-#     """
-#     Callable class that computes the sensitivity/cross_entropy ratio
-#     """
-#     def __init__(self, nb_classes: int, reduction: str = "mean"):
-#         """
-#         Sets the protected reduction method
-#
-#         Args:
-#             nb_classes: number of class in the classification problem
-#             reduction: "mean" for mean classes' sensitivity or "geometric_mean"
-#                        for geometric mean of classes' sensitivity
-#         """
-#         super().__init__(MAXIMIZE, CLASSIFICATION)
-#         self._sensitivity = Sensitivity(nb_classes, reduction)
-#         self._cross_entropy = CrossEntropyLoss(reduction)
-#
-#     def __call__(self, pred: tensor, targets: tensor) -> float:
-#         """
-#         Returns the sensitivity/cross_entropy ratio
-#
-#         Args:
-#             pred: (N,C) tensor with log probabilities
-#             targets: (N,) tensor
-#
-#         Returns: float
-#         """
-#         return self._sensitivity(pred, targets)/self._cross_entropy(pred, targets)
+class BinaryAccuracy(Metric):
+    """
+    Callable class that computes the accuracy
+    """
+    def __init__(self, n_digits: int = 5):
+        """
+        Sets protected attributes using parent's constructor
+
+        Args:
+            n_digits: number of digits kept for the score
+        """
+        super().__init__(MAXIMIZE, "Accuracy", CLASSIFICATION, n_digits)
+
+    def __call__(self, pred: tensor, targets: tensor) -> float:
+        """
+        Returns the accuracy of predictions
+
+        Args:
+            pred: (N,) tensor with predicted labels
+            targets: (N,) tensor with ground truth
+
+        Returns: float
+
+        """
+        return round((pred == targets).float().mean().item(), self.n_digits)
+
+
+class BinaryBalancedAccuracy(Metric):
+    """
+    Callable class that compute classes' sensitivity using confusion matrix
+    """
+    def __init__(self, reduction: str = "mean", n_digits: int = 5):
+        """
+        Sets the protected reduction method
+
+        Args:
+            reduction: "mean" for mean classes' sensitivity or "geometric_mean"
+                       for geometric mean of classes' sensitivity
+        """
+        assert reduction in [MEAN, GEO_MEAN], f"Reduction must be in {[MEAN, GEO_MEAN]}"
+
+        if reduction == "mean":
+            self._reduction = mean
+            name = "BalancedAcc"
+        else:
+            self._reduction = lambda x: pow(prod(x), exponent=x.shape[0])
+            name = "GeoBalancedAcc"
+
+        super().__init__(MAXIMIZE, name, CLASSIFICATION, n_digits)
+
+    def __call__(self, pred: tensor, targets: tensor) -> float:
+        """
+        Returns the classes' sensitivity
+
+        Args:
+            pred: (N,) tensor with predicted labels
+            targets: (N,) tensor with ground truth
+
+        Returns: float
+        """
+        # We get confusion matrix
+        conf_mat = self.get_confusion_matrix(pred, targets)
+        print(conf_mat)
+
+        # We get TNR and TPR
+        correct_rates = conf_mat.diag()/conf_mat.sum(dim=1)
+
+        return round(self._reduction(correct_rates).item(), self.n_digits)
+
+    @staticmethod
+    def get_confusion_matrix(pred: tensor, targets: tensor) -> tensor:
+        """
+        Returns the confusion matrix
+
+        Args:
+            pred: (N,) tensor with predicted labels
+            targets: (N,) tensor with ground truth
+
+        Returns: (2,2) tensor
+
+        """
+        # We initialize an empty confusion matrix
+        conf_matrix = zeros(2, 2)
+
+        # We fill the confusion matrix
+        for t, p in zip(targets, pred):
+            conf_matrix[t, p] += 1
+
+        return conf_matrix

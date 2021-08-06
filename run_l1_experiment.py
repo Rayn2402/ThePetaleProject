0d533152c92eaa58cf1599643e1ad681cb19464e
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-08-06 14:21:45 -0400

    file changed to execute a single call

--- run_l1_experiment.py
+++ run_l1_experiment.py
@@ -37,19 +37,12 @@ if __name__ == '__main__':
     # Creation of log file name
     log_file = f'{args.complication}.log'
 
-    # Creation of commands combinations
-    BASE_COMMANDS = ['python', FILE, '-base', '-comp', args.complication, '-han', '-logit', '-mlp',
-                     '-rf', '-xg', '-tab', '-gen']
-
-    # First set of command with significant genes
-    SIGNIFICANT_GENES_COMMAND = BASE_COMMANDS + ['significant']
-
-    # Second set of command with all genes
-    ALL_GENES_COMMAND = BASE_COMMANDS + ['all']
+    # Creation of commands
+    cmd = ['python', FILE, '-base', '-comp', args.complication, '-han', '-logit', '-mlp',
+           '-rf', '-xg', '-tab']
 
     # Run of experiments
-    for cmd in [SIGNIFICANT_GENES_COMMAND, ALL_GENES_COMMAND]:
-        check_call(cmd)
+    check_call(cmd)
 
 
 

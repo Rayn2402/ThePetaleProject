4f5944f08351b5eeb3f0211e35c05a73c824295e
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-08-06 14:22:28 -0400

    Multiple selection of genes now possible to test at once

--- experiments/learning_01/full_experiment.py
+++ experiments/learning_01/full_experiment.py
@@ -26,8 +26,9 @@ def argument_parser():
     parser.add_argument('-base', '--baselines', default=False, action='store_true',
                         help='True if we want to add baselines features into dataset')
     parser.add_argument('-comp', '--complication', type=str, default='bone',
-                        choices=['bone', 'cardio', 'neuro', 'all'], help='Choice of health complication to predict')
-    parser.add_argument('-gen', '--genes', type=str, default=None, choices=[None, 'significant', 'all'],
+                        choices=['bone', 'cardio', 'neuro', 'all'],
+                        help='Choices of health complication to predict')
+    parser.add_argument('-gen', '--genes', nargs='*', type=str, default=[None, 'significant', 'all'],
                         help="Selection of genes to incorporate into the dataset")
 
     # Models selection
@@ -92,16 +93,23 @@ if __name__ == '__main__':
     else:
         complication = COMPLICATIONS
 
+    # Extraction of genes choices
+    genes_choices = args.genes
+
     # Initialization of DataManager and sampler
     manager = PetaleDataManager("rayn2402")
 
-    # We extract data
-    df, cont_cols, cat_cols = get_learning_one_data(manager, baselines=args.baselines, genes=args.genes,
-                                                    complications=[complication])
+    # We extract data for each selection of gene
+    data_dict = {}
+    for gene in genes_choices:
+        df, cont_cols, cat_cols = get_learning_one_data(manager, baselines=args.baselines,
+                                                        genes=gene, complications=[complication])
+        data_dict[gene] = (df, cont_cols, cat_cols)
+
     # Extraction of masks
     masks = extract_masks(join(Paths.MASKS, "l1_masks.json"), k=args.nb_outer_splits, l=args.nb_inner_splits)
     masks_without_val = deepcopy(masks)
-    masks_without_val = push_valid_to_train(masks_without_val)
+    push_valid_to_train(masks_without_val)
 
     # Initialization of the dictionary containing the evaluation metrics
     evaluation_metrics = [BinaryAccuracy(), BinaryBalancedAccuracy(),
@@ -115,184 +123,196 @@ if __name__ == '__main__':
     # We start a timer for the whole experiment
     first_start = time.time()
 
-    """
-    TabNet experiment
-    """
-    if args.tabnet:
+    for gene in genes_choices:
 
-        # Start timer
-        start = time.time()
+        gene_start = time.time()
 
-        # Creation of dataset
-        dataset = PetaleDataset(df, complication, cont_cols, cat_cols)
+        """
+        TabNet experiment
+        """
+        if args.tabnet:
 
-        # Creation of function to update fixed params
-        def update_fixed_params(dts):
-            return {'cat_idx': dts.cat_idx, 'cat_sizes': dts.cat_sizes,
-                    'cat_emb_sizes': dts.cat_sizes, 'max_epochs': 250,
-                    'patience': 50}
+            # Start timer
+            start = time.time()
 
-        # Saving of original fixed params for TabNet
-        fixed_params = update_fixed_params(dataset)
+            # Creation of dataset
+            df, cont_cols, cat_cols = data_dict[gene]
+            dataset = PetaleDataset(df, complication, cont_cols, cat_cols)
 
-        # Creation of the evaluator
-        evaluator = Evaluator(model_constructor=PetaleBinaryTNC, dataset=dataset,
-                              evaluation_name=f"L1_TabNet_{args.complication}_{args.genes}",
-                              masks=masks, hps=TAB_HPS, n_trials=100, fixed_params=fixed_params,
-                              fixed_params_update_function=update_fixed_params,
-                              feature_selector=feature_selector,
-                              evaluation_metrics=evaluation_metrics,
-                              save_hps_importance=True, save_optimization_history=True)
+            # Creation of function to update fixed params
+            def update_fixed_params(dts):
+                return {'cat_idx': dts.cat_idx, 'cat_sizes': dts.cat_sizes,
+                        'cat_emb_sizes': dts.cat_sizes, 'max_epochs': 250,
+                        'patience': 50}
 
-        # Evaluation
-        evaluator.nested_cross_valid()
+            # Saving of original fixed params for TabNet
+            fixed_params = update_fixed_params(dataset)
 
-        print("Time Taken for TabNet (minutes): ", round((time.time() - start) / 60, 2))
+            # Creation of the evaluator
+            evaluator = Evaluator(model_constructor=PetaleBinaryTNC, dataset=dataset,
+                                  evaluation_name=f"L1_TabNet_{args.complication}_{gene}",
+                                  masks=masks, hps=TAB_HPS, n_trials=100, fixed_params=fixed_params,
+                                  fixed_params_update_function=update_fixed_params,
+                                  feature_selector=feature_selector,
+                                  evaluation_metrics=evaluation_metrics,
+                                  save_hps_importance=True, save_optimization_history=True)
 
-    """
-    Random Forest experiment
-    """
-    if args.random_forest:
+            # Evaluation
+            evaluator.nested_cross_valid()
 
-        # Start timer
-        start = time.time()
+            print("Time Taken for TabNet (minutes): ", round((time.time() - start) / 60, 2))
 
-        # Creation of dataset
-        dataset = PetaleDataset(df, complication, cont_cols, cat_cols)
+        """
+        Random Forest experiment
+        """
+        if args.random_forest:
 
-        # Creation of the evaluator
-        evaluator = Evaluator(model_constructor=PetaleBinaryRFC, dataset=dataset, masks=masks_without_val,
-                              evaluation_name=f"L1_RandomForest_{args.complication}_{args.genes}",
-                              hps=RF_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
-                              feature_selector=feature_selector, save_hps_importance=True,
-                              save_optimization_history=True)
+            # Start timer
+            start = time.time()
 
-        # Evaluation
-        evaluator.nested_cross_valid()
+            # Creation of dataset
+            df, cont_cols, cat_cols = data_dict[gene]
+            dataset = PetaleDataset(df, complication, cont_cols, cat_cols)
 
-        print("Time Taken for Random Forest (minutes): ", round((time.time() - start) / 60, 2))
+            # Creation of the evaluator
+            evaluator = Evaluator(model_constructor=PetaleBinaryRFC, dataset=dataset, masks=masks_without_val,
+                                  evaluation_name=f"L1_RandomForest_{args.complication}_{gene}",
+                                  hps=RF_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
+                                  feature_selector=feature_selector, save_hps_importance=True,
+                                  save_optimization_history=True)
 
-    """
-    XGBoost experiment
-    """
-    if args.xg_boost:
+            # Evaluation
+            evaluator.nested_cross_valid()
 
-        # Start timer
-        start = time.time()
+            print("Time Taken for Random Forest (minutes): ", round((time.time() - start) / 60, 2))
 
-        # Creation of dataset
-        dataset = PetaleDataset(df, complication, cont_cols, cat_cols)
+        """
+        XGBoost experiment
+        """
+        if args.xg_boost:
 
-        # Creation of the evaluator
-        evaluator = Evaluator(model_constructor=PetaleBinaryXGBC, dataset=dataset, masks=masks_without_val,
-                              evaluation_name=f"L1_XGBoost_{args.complication}_{args.genes}",
-                              hps=XGBOOST_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
-                              feature_selector=feature_selector, save_hps_importance=True,
-                              save_optimization_history=True)
+            # Start timer
+            start = time.time()
 
-        # Evaluation
-        evaluator.nested_cross_valid()
+            # Creation of dataset
+            df, cont_cols, cat_cols = data_dict[gene]
+            dataset = PetaleDataset(df, complication, cont_cols, cat_cols)
 
-        print("Time Taken for XGBoost (minutes): ", round((time.time() - start) / 60, 2))
+            # Creation of the evaluator
+            evaluator = Evaluator(model_constructor=PetaleBinaryXGBC, dataset=dataset, masks=masks_without_val,
+                                  evaluation_name=f"L1_XGBoost_{args.complication}_{gene}",
+                                  hps=XGBOOST_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
+                                  feature_selector=feature_selector, save_hps_importance=True,
+                                  save_optimization_history=True)
 
-    """
-    MLP experiment
-    """
-    if args.mlp:
+            # Evaluation
+            evaluator.nested_cross_valid()
 
-        # Start timer
-        start = time.time()
+            print("Time Taken for XGBoost (minutes): ", round((time.time() - start) / 60, 2))
 
-        # Creation of the dataset
-        dataset = PetaleDataset(df, complication, cont_cols, cat_cols, to_tensor=True)
+        """
+        MLP experiment
+        """
+        if args.mlp:
 
-        # Creation of function to update fixed params
-        def update_fixed_params(dts):
-            return {'max_epochs': 250, 'patience': 50, 'num_cont_col': len(dts.cont_cols),
-                    'cat_idx': dts.cat_idx, 'cat_sizes': dts.cat_sizes,
-                    'cat_emb_sizes': dts.cat_sizes}
+            # Start timer
+            start = time.time()
 
-        # Saving of fixed_params for MLP
-        fixed_params = update_fixed_params(dataset)
+            # Creation of the dataset
+            df, cont_cols, cat_cols = data_dict[gene]
+            dataset = PetaleDataset(df, complication, cont_cols, cat_cols, to_tensor=True)
 
-        # Creation of evaluator
-        evaluator = Evaluator(model_constructor=PetaleBinaryMLPC, dataset=dataset, masks=masks,
-                              evaluation_name=f"L1_MLP_{args.complication}_{args.genes}",
-                              hps=MLP_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
-                              feature_selector=feature_selector, fixed_params=fixed_params,
-                              fixed_params_update_function=update_fixed_params,
-                              save_hps_importance=True, save_optimization_history=True)
+            # Creation of function to update fixed params
+            def update_fixed_params(dts):
+                return {'max_epochs': 250, 'patience': 50, 'num_cont_col': len(dts.cont_cols),
+                        'cat_idx': dts.cat_idx, 'cat_sizes': dts.cat_sizes,
+                        'cat_emb_sizes': dts.cat_sizes}
 
-        # Evaluation
-        evaluator.nested_cross_valid()
+            # Saving of fixed_params for MLP
+            fixed_params = update_fixed_params(dataset)
 
-        print("Time Taken for MLP (minutes): ", round((time.time() - start) / 60, 2))
+            # Creation of evaluator
+            evaluator = Evaluator(model_constructor=PetaleBinaryMLPC, dataset=dataset, masks=masks,
+                                  evaluation_name=f"L1_MLP_{args.complication}_{gene}",
+                                  hps=MLP_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
+                                  feature_selector=feature_selector, fixed_params=fixed_params,
+                                  fixed_params_update_function=update_fixed_params,
+                                  save_hps_importance=True, save_optimization_history=True)
 
-    """
-    HAN experiment
-    """
-    if args.han:
+            # Evaluation
+            evaluator.nested_cross_valid()
 
-        # Start timer
-        start = time.time()
+            print("Time Taken for MLP (minutes): ", round((time.time() - start) / 60, 2))
 
-        # Creation of the dataset
-        dataset = PetaleStaticGNNDataset(df, complication, cont_cols, cat_cols)
+        """
+        HAN experiment
+        """
+        if args.han:
 
-        # Creation of function to update fixed params
-        def update_fixed_params(dts):
-            return {'meta_paths': dts.get_metapaths(), 'in_size': len(dts.cont_cols),
-                    'max_epochs': 250, 'patience': 15}
+            # Start timer
+            start = time.time()
 
+            # Creation of the dataset
+            df, cont_cols, cat_cols = data_dict[gene]
+            dataset = PetaleStaticGNNDataset(df, complication, cont_cols, cat_cols)
 
-        # Saving of original fixed params for HAN
-        fixed_params = update_fixed_params(dataset)
-
-        # Creation of the evaluator
-        evaluator = Evaluator(model_constructor=PetaleBinaryHANC, dataset=dataset, masks=masks,
-                              evaluation_name=f"L1_HAN_{args.complication}_{args.genes}",
-                              hps=HAN_HPS, n_trials=50, evaluation_metrics=evaluation_metrics,
-                              fixed_params=fixed_params, fixed_params_update_function=update_fixed_params,
-                              feature_selector=feature_selector, save_hps_importance=True,
-                              save_optimization_history=True)
-
-        # Evaluation
-        evaluator.nested_cross_valid()
-
-        print("Time Taken for HAN (minutes): ", round((time.time() - start) / 60, 2))
-
-    """
-    Logistic regression experiment
-    """
-    if args.logistic_regression:
-
-        # Start timer
-        start = time.time()
-
-        # Creation of the dataset
-        dataset = PetaleDataset(df, complication, cont_cols, cat_cols, to_tensor=True)
-
-        # Creation of function to update fixed params
-        def update_fixed_params(dts):
-            return {'max_epochs': 1500, 'patience': 200, 'num_cont_col': len(dts.cont_cols),
-                    'cat_idx': dts.cat_idx, 'cat_sizes': dts.cat_sizes,
-                    'cat_emb_sizes': dts.cat_sizes}
+            # Creation of function to update fixed params
+            def update_fixed_params(dts):
+                return {'meta_paths': dts.get_metapaths(), 'in_size': len(dts.cont_cols),
+                        'max_epochs': 250, 'patience': 15}
 
 
-        # Saving of fixed_params for MLP
-        fixed_params = update_fixed_params(dataset)
+            # Saving of original fixed params for HAN
+            fixed_params = update_fixed_params(dataset)
 
-        # Creation of evaluator
-        evaluator = Evaluator(model_constructor=PetaleBinaryMLPC, dataset=dataset, masks=masks_without_val,
-                              evaluation_name=f"L1_Logit_{args.complication}_{args.genes}",
-                              hps=LOGIT_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
-                              feature_selector=feature_selector, fixed_params=fixed_params,
-                              fixed_params_update_function=update_fixed_params,
-                              save_hps_importance=True, save_optimization_history=True)
+            # Creation of the evaluator
+            evaluator = Evaluator(model_constructor=PetaleBinaryHANC, dataset=dataset, masks=masks,
+                                  evaluation_name=f"L1_HAN_{args.complication}_{gene}",
+                                  hps=HAN_HPS, n_trials=50, evaluation_metrics=evaluation_metrics,
+                                  fixed_params=fixed_params, fixed_params_update_function=update_fixed_params,
+                                  feature_selector=feature_selector, save_hps_importance=True,
+                                  save_optimization_history=True)
 
-        # Evaluation
-        evaluator.nested_cross_valid()
+            # Evaluation
+            evaluator.nested_cross_valid()
 
-        print("Time Taken for Logistic Regression (minutes): ", round((time.time() - start) / 60, 2))
+            print("Time Taken for HAN (minutes): ", round((time.time() - start) / 60, 2))
+
+        """
+        Logistic regression experiment
+        """
+        if args.logistic_regression:
+
+            # Start timer
+            start = time.time()
+
+            # Creation of the dataset
+            df, cont_cols, cat_cols = data_dict[gene]
+            dataset = PetaleDataset(df, complication, cont_cols, cat_cols, to_tensor=True)
+
+            # Creation of function to update fixed params
+            def update_fixed_params(dts):
+                return {'max_epochs': 1500, 'patience': 200, 'num_cont_col': len(dts.cont_cols),
+                        'cat_idx': dts.cat_idx, 'cat_sizes': dts.cat_sizes,
+                        'cat_emb_sizes': dts.cat_sizes}
+
+
+            # Saving of fixed_params for MLP
+            fixed_params = update_fixed_params(dataset)
+
+            # Creation of evaluator
+            evaluator = Evaluator(model_constructor=PetaleBinaryMLPC, dataset=dataset, masks=masks_without_val,
+                                  evaluation_name=f"L1_Logit_{args.complication}_{gene}",
+                                  hps=LOGIT_HPS, n_trials=100, evaluation_metrics=evaluation_metrics,
+                                  feature_selector=feature_selector, fixed_params=fixed_params,
+                                  fixed_params_update_function=update_fixed_params,
+                                  save_hps_importance=True, save_optimization_history=True)
+
+            # Evaluation
+            evaluator.nested_cross_valid()
+
+            print("Time Taken for Logistic Regression (minutes): ", round((time.time() - start) / 60, 2))
+
+        print(f"\nTime Taken for Genes = {gene}  (minutes): ", round((time.time() - gene_start) / 60, 2), "\n")
 
     print("Overall time (minutes): ", round((time.time() - first_start) / 60, 2))

c6fa483dde4accad0fdf310fb74c1cabbf6b5731
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-08-30 21:46:06 -0400

    orginal equation experiment updated

--- experiments/warmup/original_equation.py
+++ experiments/warmup/original_equation.py
@@ -11,9 +11,9 @@ import sys
 sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 from src.data.extraction.data_management import PetaleDataManager
 from src.data.processing.sampling import TRAIN, TEST, get_warmup_data, extract_masks
-from src.data.processing.datasets import PetaleRFDataset
+from src.data.processing.datasets import PetaleDataset
 from src.data.processing.transforms import ContinuousTransform, CategoricalTransform
-from src.utils.score_metrics import RootMeanSquaredError, AbsoluteError
+from src.utils.score_metrics import AbsoluteError, Pearson, RootMeanSquaredError, SquaredError
 from src.data.extraction.constants import *
 from torch import tensor
 from src.recording.recording import Recorder, get_evaluation_recap, compare_prediction_recordings
@@ -36,8 +36,8 @@ def argument_parser():
     parser = argparse.ArgumentParser(usage='\n python3 original_equation.py',
                                      description="Runs the original equation experiment")
 
-    parser.add_argument('-nos', '--nb_outer_splits', type=int, default=20,
-                        help="Number of outer splits (default = 20)")
+    parser.add_argument('-k', '--nb_outer_splits', type=int, default=5,
+                        help="Number of outer splits (default = 5)")
 
     parser.add_argument('-u', '--user', type=str, default='rayn2402',
                         help="Valid username for petale database")
@@ -53,63 +53,65 @@ def argument_parser():
     return arguments
 
 
-def execute_original_equation_experiment(dataset: PetaleRFDataset,
-                                         masks: Dict[int, Dict[str, List[int]]],
-                                         evaluation_name:str) -> None:
+def execute_original_equation_experiment(dts: PetaleDataset,
+                                         m: Dict[int, Dict[str, List[int]]],
+                                         eval_name: str) -> None:
     """
         Function that executes a Neural Network experiments
 
          Args:
-            dataset:  dataset with inputs and regression targets
-            masks: dictionary with list of idx to use for training and testing
-            evaluation_name: name of the results file saved at the recordings_path
+            dts:  dataset with inputs and regression targets
+            m: dictionary with list of idx to use for training and testing
+            eval_name: name of the results file saved at the recordings_path
 
 
          Returns: None
 
          """
-    # We save the metric object
-    metric = RootMeanSquaredError()
-
     # We save the evaluation metrics
-    eval_metrics = {'MAE': AbsoluteError(), 'RMSE': metric}
+    eval_metrics = [AbsoluteError(), Pearson(), SquaredError(), RootMeanSquaredError()]
 
-    # We run training and testing for each masks
-    for k, v in masks.items():
+    # We run tests for each masks
+    for k, v in m.items():
 
         # Masks extraction and dataset update
         train_mask, test_mask = v[TRAIN], v[TEST]
-        dataset.update_masks(train_mask=train_mask, test_mask=test_mask)
+        dts.update_masks(train_mask=train_mask, test_mask=test_mask)
 
         # Data extraction and preprocessing without normalization
-        x = dataset.original_data.copy()
-        mu, _, _ = dataset.current_train_stats()
-        x = ContinuousTransform.fill_missing(x, mu)
-
-        # Train and test data extraction
-        x_test = x.iloc[test_mask]
-        _, y_test = dataset[test_mask]
+        x_copy = dts.original_data.copy()
+        mu, _, _ = dts.current_train_stats()
+        x_copy = ContinuousTransform.fill_missing(x_copy, mu)
 
         # Recorder initialization
-        recorder = Recorder(evaluation_name=evaluation_name,
+        recorder = Recorder(evaluation_name=eval_name,
                             index=k, recordings_path=Paths.EXPERIMENTS_RECORDS)
 
         # Prediction calculations and recording
-        predictions = []
-        for i, row in x_test.iterrows():
-            predictions.append(original_equation(row))
-        predictions = tensor(predictions)
-        recorder.record_predictions([dataset.ids[i] for i in test_mask], predictions, y_test)
+        for mask, test in [(train_mask, False), (test_mask, True)]:
 
-        # Score calculation and recording
-        for metric_name, f in eval_metrics.items():
-            recorder.record_scores(score=f(predictions, y_test), metric=metric_name)
+            # Data extraction
+            x = x_copy.iloc[mask]
+            _, y, _ = dts[mask]
+
+            # Prediction
+            predictions = []
+            for i, row in x.iterrows():
+                predictions.append(original_equation(row))
+
+            predictions = tensor(predictions)
+            recorder.record_predictions([dataset.ids[i] for i in mask], predictions, y, test)
+
+            # Score calculation and recording
+            for metric in eval_metrics:
+                recorder.record_scores(score=metric(predictions, y), metric=metric.name, test=test)
 
         # Generation of the file with the results
         recorder.generate_file()
-        compare_prediction_recordings(evaluations=[evaluation_name], split_index=k,
+        compare_prediction_recordings(evaluations=[eval_name], split_index=k,
                                       recording_path=Paths.EXPERIMENTS_RECORDS)
-    get_evaluation_recap(evaluation_name=evaluation_name, recordings_path=Paths.EXPERIMENTS_RECORDS)
+
+    get_evaluation_recap(evaluation_name=eval_name, recordings_path=Paths.EXPERIMENTS_RECORDS)
 
 
 if __name__ == '__main__':
@@ -125,11 +127,12 @@ if __name__ == '__main__':
     df, target, cont_cols, _ = get_warmup_data(data_manager)
 
     # Creation of the dataset
-    dataset = PetaleRFDataset(df, target, cont_cols, cat_cols=None)
+    dataset = PetaleDataset(df, target, cont_cols, cat_cols=None,
+                            classification=False, to_tensor=True)
 
     # Extraction of masks
-    masks = extract_masks(join(Paths.MASKS, "l0_masks.json"), k=k, l=0)
+    masks = extract_masks(join(Paths.MASKS, "l0_warmup_genes.json"), k=k, l=0)
 
     # Execution of the experiment
-    evaluation_name = f"original_equation_k{k}"
-    execute_original_equation_experiment(dataset=dataset, masks=masks, evaluation_name=evaluation_name)
+    evaluation_name = f"original_equation"
+    execute_original_equation_experiment(dts=dataset, m=masks, eval_name=evaluation_name)

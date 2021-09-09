8a8fe47328599c42c136ed79051b2f636a4e0d86
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-09-09 13:02:18 -0400

    New options added to match with full_experiment file

--- run_warmup_experiment.py
+++ run_warmup_experiment.py
@@ -5,15 +5,52 @@ File use to run all warmup experiments
 from os.path import join
 from settings.paths import Paths
 from subprocess import check_call
+import argparse
+
+
+def argument_parser():
+    """
+    This function defines a parser that enables user to easily run different experiments
+    """
+    # Create a parser
+    parser = argparse.ArgumentParser(usage='\n python run_warmup_experiment.py',
+                                     description="Runs all the experiments associated to the warmup dataset")
+
+    # Features selection
+    parser.add_argument('-gen', '--genes', default=False, action='store_true',
+                        help='True if we want to include genes if features')
+    parser.add_argument('-f', '--feature_selection', default=False, action='store_true',
+                        help='True if we want to apply automatic feature selection')
+    parser.add_argument('-s', '--sex', default=False, action='store_true',
+                        help='True if we want to include sex in features')
+
+    arguments = parser.parse_args()
+
+    # Print arguments
+    print("\nThe inputs are:")
+    for arg in vars(arguments):
+        print("{}: {}".format(arg, getattr(arguments, arg)))
+    print("\n")
+
+    return arguments
 
 
 if __name__ == '__main__':
+
+    # Arguments parsing
+    args = argument_parser()
 
     # Extraction of file path
     FILE = str(join(Paths.WARMUP_EXPERIMENTS_SCRIPTS, "full_experiment.py"))
 
     # Creation of commands
     cmd = ['python', FILE, '-lin', '-han', '-mlp', '-rf', '-xg', '-tab']
+    if args.genes:
+        cmd.append('-gen')
+    if args.feature_selection:
+        cmd.append('-f')
+    if args.sex:
+        cmd.append('-s')
 
     # Run of experiments
     check_call(cmd)

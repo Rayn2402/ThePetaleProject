b662f3107514bdea75c1f22839669feb2cb7e421
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-09-24 15:40:06 -0400

    Label propagation methods added

--- src/utils/graph.py
+++ src/utils/graph.py
@@ -7,8 +7,9 @@ This file is used to store the PetaleGraph class
 from numpy import array, cov
 from numpy.linalg import inv
 from src.data.processing.datasets import PetaleDataset
+from src.data.processing.sampling import TRAIN, VALID, TEST
 from torch import tensor, zeros, transpose, diag, pow, mm, eye, is_tensor
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 
 class PetaleGraph:
@@ -38,6 +39,25 @@ class PetaleGraph:
         else:
             self.weight = mm(diag(pow(self.degrees, -1)), self.edges)  # Inverse of degrees multiplied by edges
 
+    def propagate_labels(self, labels: tensor, r: float, nb_iter: int):
+        """
+        Applies iterative label propagation algorithm
+
+        Args:
+            labels: (N, ) tensor with ground truth
+            r: proportion attributed to original labels throughout iteration
+               (restart probability)
+            nb_iter: nb of propagation iteration
+
+        Returns: (N, ) tensor with new labels after propagation
+        """
+        assert 0 <= r <= 1, "r must be in range [0, 1]"
+        final_labels = labels
+        for i in range(nb_iter):
+            final_labels = r*labels + (1-r)*mm(self.weight, final_labels)
+
+        return final_labels
+
     def random_walk_with_restart(self, walk_length: int, restart_proba: float) -> tensor:
         """
         Approximate stationary distribution of each node using a random walk
@@ -58,37 +78,6 @@ class PetaleGraph:
             p = restart_proba*p_init + (1 - restart_proba)*mm(p, self.weight)
 
         return p
-
-    @staticmethod
-    def compute_distances(dataset: PetaleDataset) -> tensor:
-        """
-        Calculates mahalanobis distances between individuals
-        Args:
-            dataset: custom dataset with training and test data
-
-        Returns: (N, N) tensor with distances
-        """
-        # We first compute inverse of covariance matrix related to numerical columns
-        x, _, _ = dataset[:]
-        numerical_data = x[:, dataset.cont_idx]
-        if is_tensor(numerical_data):
-            numerical_data = numerical_data.numpy()
-        cov_mat = tensor(inv(cov(numerical_data, rowvar=False))).float()
-
-        # We convert numerical data to tensor
-        numerical_data = tensor(numerical_data).float()
-
-        # We compute squared mahalanobis distances
-        mahalanobis_dist = zeros(len(dataset), len(dataset), requires_grad=False)
-        for i in range(len(dataset)):
-            for j in range(i+1, len(dataset)):
-                diff = (numerical_data[i, :] - numerical_data[j, :]).reshape(-1, 1)
-                mahalanobis_dist[i, j] = mm(mm(transpose(diff, 0, 1), cov_mat), diff)
-
-        # We add the matrix to its transpose to have all distances
-        mahalanobis_dist = mahalanobis_dist + transpose(mahalanobis_dist, 0, 1)
-
-        return mahalanobis_dist
 
     @staticmethod
     def build_edges_matrix(dataset: PetaleDataset, cat_cols: Optional[List[str]] = None) -> tensor:
@@ -129,3 +118,71 @@ class PetaleGraph:
         edges = edges + transpose(edges, 0, 1)
 
         return edges
+
+    @staticmethod
+    def compute_distances(dataset: PetaleDataset) -> tensor:
+        """
+        Calculates mahalanobis distances between individuals
+        Args:
+            dataset: custom dataset with training and test data
+
+        Returns: (N, N) tensor with distances
+        """
+        # We first compute inverse of covariance matrix related to numerical columns
+        x, _, _ = dataset[:]
+        numerical_data = x[:, dataset.cont_idx]
+        if is_tensor(numerical_data):
+            numerical_data = numerical_data.numpy()
+        cov_mat = tensor(inv(cov(numerical_data, rowvar=False))).float()
+
+        # We convert numerical data to tensor
+        numerical_data = tensor(numerical_data).float()
+
+        # We compute squared mahalanobis distances
+        mahalanobis_dist = zeros(len(dataset), len(dataset), requires_grad=False)
+        for i in range(len(dataset)):
+            for j in range(i+1, len(dataset)):
+                diff = (numerical_data[i, :] - numerical_data[j, :]).reshape(-1, 1)
+                mahalanobis_dist[i, j] = mm(mm(transpose(diff, 0, 1), cov_mat), diff)
+
+        # We add the matrix to its transpose to have all distances
+        mahalanobis_dist = mahalanobis_dist + transpose(mahalanobis_dist, 0, 1)
+
+        return mahalanobis_dist
+
+
+def correct_and_smooth(g: PetaleGraph, pred: tensor, labels: tensor, masks: Dict[str, Optional[List[int]]],
+                       r: float, nb_iter: int) -> tensor:
+    """
+    Applies correction and smoothing to the original
+    Args:
+        g: PetaleGraph
+        pred: (N, ) tensor with predictions
+        labels: (N, ) tensor with real labels (ground truth)
+        masks: Dictionary with train, valid and test mask (at least train and test)
+        r: proportion attributed to original labels throughout iteration
+               (restart probability)
+        nb_iter: nb of propagation iteration
+
+    Returns: (N, ) tensor with corrected and smoothed labels
+
+    """
+    assert (TRAIN in masks.keys()) and (TEST in masks.keys()),\
+        f"{TRAIN} and {TEST} must be in dictionary's keys"
+
+    # We extract train and valid masks
+    labeled_mask = masks[TRAIN]
+    if masks.get(VALID) is not None:
+        labeled_mask += masks[VALID]
+
+    # We propagate the errors
+    errors = zeros(size=(len(labels), 1))
+    errors[labeled_mask] = pred[labeled_mask] - labels[labeled_mask]
+    errors = g.propagate_labels(errors, r, nb_iter)
+
+    # We smooth labels
+    test_mask = masks[TEST]
+    labels[test_mask] = pred[test_mask] - errors[test_mask]
+    labels = g.propagate_labels(labels, r, nb_iter)
+
+    return labels

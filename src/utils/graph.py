793c79e985e75e637ec7db5e7361dac247731208
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-09-28 12:06:52 -0400

    max_degree option added

--- src/utils/graph.py
+++ src/utils/graph.py
@@ -8,7 +8,7 @@ from numpy import array, cov
 from numpy.linalg import inv
 from src.data.processing.datasets import PetaleDataset
 from src.data.processing.sampling import TRAIN, VALID, TEST
-from torch import tensor, zeros, transpose, diag, pow, mm, eye, is_tensor
+from torch import tensor, zeros, transpose, diag, pow, mm, eye, is_tensor, ones, topk
 from typing import Dict, List, Optional
 
 
@@ -17,13 +17,15 @@ class PetaleGraph:
     Represents an homogeneous undirected graph
     """
     def __init__(self, dataset: PetaleDataset, cat_cols: Optional[List[str]] = None,
-                 include_distances: bool = False):
+                 include_distances: bool = False, max_degree: Optional[int] = None):
         """
         Sets the edges, adjacency, weight and degrees matrix
         Args:
             dataset: custom dataset with training and test data
-            cat_cols: list of categorical columns from which to build the graph
+            cat_cols: list of categorical columns from which to build the graph, if None
+                      all nodes are connected together.
             include_distances: True if we want to use distance between patient to define edges weights
+            max_degree: Maximum number of degree that a node can have, if None there is no limit.
         """
         
         self.edges = self.build_edges_matrix(dataset, cat_cols)
@@ -33,11 +35,31 @@ class PetaleGraph:
             # We first set the weights as 1/distance
             self.weight = 1/(self.compute_distances(dataset) + eye(self.edges.shape[0])) - eye(self.edges.shape[0])
 
+            # We multiply the weights by the edges matrix to remove some connections
+            self.weight *= self.edges
+
             # We normalize the weights on each row
-            self.weight /= self.weight.sum(axis=1)
+            self.weight /= self.weight.sum(axis=1).reshape(-1, 1)
 
         else:
             self.weight = mm(diag(pow(self.degrees, -1)), self.edges)  # Inverse of degrees multiplied by edges
+
+        # If a max degree is given we only keep the "max_degree" closest neighbors
+        if max_degree is not None:
+
+            # We find the top_k idx for each rows
+            _, top_k_idx = topk(self.weight, k=max_degree, dim=1)
+
+            # We create a filter for top_k neighbors of each row
+            top_k_mask = zeros((len(dataset), len(dataset)))
+            for i in range(top_k_idx.shape[0]):
+                top_k_mask[i, top_k_idx[i, :]] = 1
+
+            # We multiply weights by the filter (Hadamard product)
+            self.weight *= top_k_mask
+
+            # We normalize the weights on each row
+            self.weight /= self.weight.sum(axis=1).reshape(-1, 1)
 
     def propagate_labels(self, labels: tensor, r: float, nb_iter: int):
         """
@@ -97,7 +119,7 @@ class PetaleGraph:
             for c in cat_cols:
                 assert c in dataset.cat_cols, f"Unrecognized categorical column name : {c}"
         else:
-            cat_cols = dataset.cat_cols
+            return ones(size=(len(dataset), len(dataset))) - eye(len(dataset))
 
         # We fill the upper triangle of the edges matrix
         df = dataset.original_data[cat_cols]
@@ -133,7 +155,7 @@ class PetaleGraph:
         numerical_data = x[:, dataset.cont_idx]
         if is_tensor(numerical_data):
             numerical_data = numerical_data.numpy()
-        cov_mat = tensor(inv(cov(numerical_data, rowvar=False))).float()
+        inv_cov_mat = tensor(inv(cov(numerical_data, rowvar=False))).float()
 
         # We convert numerical data to tensor
         numerical_data = tensor(numerical_data).float()
@@ -143,7 +165,7 @@ class PetaleGraph:
         for i in range(len(dataset)):
             for j in range(i+1, len(dataset)):
                 diff = (numerical_data[i, :] - numerical_data[j, :]).reshape(-1, 1)
-                mahalanobis_dist[i, j] = mm(mm(transpose(diff, 0, 1), cov_mat), diff)
+                mahalanobis_dist[i, j] = mm(mm(transpose(diff, 0, 1), inv_cov_mat), diff)
 
         # We add the matrix to its transpose to have all distances
         mahalanobis_dist = mahalanobis_dist + transpose(mahalanobis_dist, 0, 1)
@@ -176,6 +198,7 @@ def correct_and_smooth(g: PetaleGraph, pred: tensor, labels: tensor, masks: Dict
         labeled_mask += masks[VALID]
 
     # We propagate the errors
+    labels = labels.reshape(-1, 1)
     errors = zeros(size=(len(labels), 1))
     errors[labeled_mask] = pred[labeled_mask] - labels[labeled_mask]
     errors = g.propagate_labels(errors, r, nb_iter)
@@ -185,4 +208,4 @@ def correct_and_smooth(g: PetaleGraph, pred: tensor, labels: tensor, masks: Dict
     labels[test_mask] = pred[test_mask] - errors[test_mask]
     labels = g.propagate_labels(labels, r, nb_iter)
 
-    return labels
+    return labels.flatten()

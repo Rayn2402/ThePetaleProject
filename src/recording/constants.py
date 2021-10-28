5cc5c1463d59df31252afe4f760e54898145c52d
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-10-28 09:52:24 -0400

    header docstring changed

--- src/recording/constants.py
+++ src/recording/constants.py
@@ -1,5 +1,12 @@
 """
-This file is used to store useful constants related to the Recorder class
+Filename: constants.py
+
+Authors: Nicolas Raymond
+         Mehdi Mitiche
+
+Description: This file is used to store useful constants related to the Recorder class
+
+Date of last modification : 2021/10/28
 """
 
 NAME = "name"

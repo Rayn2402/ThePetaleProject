3189270ca300825ac23ffe3182f8c59fdc6f883c
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-11-16 13:18:58 -0500

    constants added

--- src/training/sam.py
+++ src/training/sam.py
@@ -18,6 +18,11 @@ class SAM(torch.optim.Optimizer):
     """
     Sharpness-Aware Minimization Optimizer
     """
+    # Constants
+    ADAPTIVE = "adaptive"
+    PARAMS = "params"
+    OLD_P = "old_p"
+
     def __init__(self, params: Iterator,
                  base_optimizer: Callable,
                  rho: float = 0.05,
@@ -52,12 +57,12 @@ class SAM(torch.optim.Optimizer):
         Returns: norm
         """
         # put everything on the same device, in case of model parallelism
-        shared_device = self.param_groups[0]["params"][0].device
+        shared_device = self.param_groups[0][SAM.PARAMS][0].device
 
         norm = torch.norm(
             torch.stack([
-                ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
-                for group in self.param_groups for p in group["params"]
+                ((torch.abs(p) if group[SAM.ADAPTIVE] else 1.0) * p.grad).norm(p=2).to(shared_device)
+                for group in self.param_groups for p in group[SAM.PARAMS]
                 if p.grad is not None
             ]),
             p=2
@@ -65,13 +70,10 @@ class SAM(torch.optim.Optimizer):
         return norm
 
     @torch.no_grad()
-    def first_step(self, zero_grad: bool = False) -> None:
+    def first_step(self) -> None:
         """
         Performs the first optimization step that finds the weights
         with the highest loss in the local rho-neighborhood.
-
-        Args:
-            zero_grad: true if we want to set gradients to zero at the end of the step
 
         Returns: None
 
@@ -80,36 +82,30 @@ class SAM(torch.optim.Optimizer):
         for group in self.param_groups:
             scale = group["rho"] / (grad_norm + 1e-12)
 
-            for p in group["params"]:
+            for p in group[SAM.PARAMS]:
                 if p.grad is not None:
-                    self.state[p]["old_p"] = p.data.clone()
-                    e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+                    self.state[p][SAM.OLD_P] = p.data.clone()
+                    e_w = (torch.pow(p, 2) if group[SAM.ADAPTIVE] else 1.0) * p.grad * scale.to(p)
                     p.add_(e_w)  # climb to the local maximum "w + e(w)"
 
-        if zero_grad:
-            self.zero_grad()
+        self.zero_grad()
 
     @torch.no_grad()
-    def second_step(self, zero_grad: bool = False) -> None:
+    def second_step(self) -> None:
         """
         Performs the second optimization step that updates the original weights
         with the gradient from the (locally) highest point in the loss landscape.
-
-        Args:
-            zero_grad: true if we want to set gradients to zero at the end of the step
 
         Returns: None
 
         """
         for group in self.param_groups:
-            for p in group["params"]:
+            for p in group[SAM.PARAMS]:
                 if p.grad is not None:
-                    p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
+                    p.data = self.state[p][SAM.OLD_P]  # get back to "w" from "w + e(w)"
 
         self._base_optimizer.step()  # do the actual "sharpness-aware" update
-
-        if zero_grad:
-            self.zero_grad()
+        self.zero_grad()
 
     def load_state_dict(self, state_dict: OrderedDict[str, torch.tensor]) -> None:
         """

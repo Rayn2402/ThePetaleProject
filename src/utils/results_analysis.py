0fb2f6697780bb6187f22c367b8b2e4f37fd9715
Rayn2402 <rayn2402@usherbrooke.ca>
2021-12-02 13:37:20 -0500

    New function to create csv with test metrics

--- src/utils/results_analysis.py
+++ src/utils/results_analysis.py
@@ -11,9 +11,25 @@ Date of last modification: 2021/12/01
 from json import dump, load
 from numpy import mean, std
 from os import listdir
-from os.path import join
+from os.path import join, isdir
+from pandas import DataFrame
+from settings.paths import Paths
+from src.recording.constants import MEAN, STD, SUMMARY_FILE, TEST_METRICS
+from typing import List
 
 APRIORI_KEYS = ['Support', 'Lift', 'Confidence']
+
+
+def get_directories(path: str) -> List[str]:
+    """
+    Extracts the names of all the folders that can be found at the given path
+
+    Args:
+        path: directory
+
+    Returns: list of folder names (str)
+    """
+    return [folder for folder in listdir(path) if isdir(join(path, folder))]
 
 
 def get_apriori_statistics(path: str) -> None:
@@ -59,5 +75,45 @@ def get_apriori_statistics(path: str) -> None:
     # We save the summary in a json file
     with open(join(path, "summary.json"), "w") as file:
         dump(rules_statistics, file, indent=True)
+
+
+def get_experiment_summaries(path: str, csv_filename: str, nb_digits: int = 2) -> None:
+    """
+    From a folder containing experiment results of different models,
+    we extract test metrics and save it in a csv that can then be converted to a LaTeX table using :
+
+    https://www.tablesgenerator.com/
+
+    Args:
+        path: directory where the experiment results are stored
+        csv_filename: name of the csv file containing the extracted data
+        nb_digits: number of digits kept in the csv
+
+    Returns: None
+    """
+    # We extract the names of all the folders in the directory
+    folders = get_directories(path)
+
+    # We extract test metrics data from each model directory
+    test_metrics = {}
+    for f in folders:
+
+        # We extract the name of the model
+        m = f.split('_')[0]
+
+        # We load the data from the summary
+        with open(join(path, f, SUMMARY_FILE), "r") as read_file:
+            data = load(read_file)
+
+        # We save the metrics in the dictionary
+        test_metrics[m] = {metric: f"{round(float(v[MEAN]), nb_digits)} +- {round(float(v[STD]), nb_digits)}"
+                           for metric, v in data[TEST_METRICS].items()}
+
+    # We store the data in a dataframe and save it into a csv
+    df = DataFrame(data=test_metrics).T
+    df.to_csv(path_or_buf=join(Paths.CSV_FILES, f"{csv_filename}.csv"))
+
+
+
 
 

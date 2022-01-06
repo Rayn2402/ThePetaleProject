a6e8f68bad2342f06af34ff1bc0578b1e65896db
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-01-06 10:49:01 -0500

    GeneSignatureDecoder class started

--- src/models/blocks/genes_signature_block.py
+++ src/models/blocks/genes_signature_block.py
@@ -10,11 +10,11 @@ Date of last modification: 2022/01/04
 """
 
 from src.models.abstract_models.encoder import Encoder
-from src.models.blocks.mlp_blocks import EntityEmbeddingBlock
+from src.models.blocks.mlp_blocks import BaseBlock, EntityEmbeddingBlock
 from torch import einsum, tensor, zeros
 from torch.nn import BatchNorm1d, Conv1d, Linear, Module
 from torch.nn.functional import relu
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 
 class GeneGraphEncoder(Encoder, Module):
@@ -141,5 +141,62 @@ class GeneGraphEncoder(Encoder, Module):
         return h
 
 
+class GeneSignatureDecoder(Module):
+    """
+    From a signature given by the GeneGraphEncoder, this module tries to recover
+    the original graph adjacency matrix
+    """
+    def __init__(self,
+                 genes_idx_group: Dict[str, List[int]],
+                 signature_size: int = 10):
 
+        """
+        Builds the BaseBlock layer and saves the adjacency matrix related
+        to the genome of patient
 
+        Args:
+            genes_idx_group: dictionary where keys are names of chromosomes and values
+                             are list of idx referring to columns of genes associated to
+                             the chromosome
+
+            signature_size: genomic signature size (input size)
+        """
+
+        # Call of parent's constructor
+        super().__init__()
+
+        # Count of the number of genes and creation of the adjacency matrix
+        self.__nb_genes, self.__adj_mat = self.__set_adjacency_mat(genes_idx_group)
+
+        # Creation of BaseBlock (first layer of the decoder)
+        self._linear_layer = BaseBlock(input_size=signature_size,
+                                       output_size=self.__nb_genes,
+                                       activation='ReLU')
+
+    @staticmethod
+    def __set_adjacency_mat(genes_idx_group: Dict[str, List[int]]) -> Tuple[int, tensor]:
+        """
+        Builds the adjacency matrix related to the genome graph identical
+        to all patients
+
+        Args:
+            genes_idx_group: dictionary where keys are names of chromosomes and values
+                             are list of idx referring to columns of genes associated to
+                             the chromosome
+
+        Returns: nb of genes, adjacency matrix
+        """
+        nb_genes = 0
+        x_coords, y_coords = [], []
+        for idx in genes_idx_group.values():
+            nb_genes_in_chrom = len(idx)
+            for i, x in enumerate(range(nb_genes, (nb_genes + nb_genes_in_chrom - 1))):
+                x_coords += [x]*(nb_genes_in_chrom - i - 1)
+                y_coords += range(x + 1, nb_genes + nb_genes_in_chrom)
+            nb_genes += nb_genes_in_chrom
+
+        adj_mat = zeros(nb_genes, nb_genes)
+        adj_mat[x_coords, y_coords] = 1
+        adj_mat += adj_mat.t()
+
+        return nb_genes, adj_mat

fe140f9b2df04f65bef6074dd751db8ce7f215b2
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-01-20 11:44:46 -0500

    .name added to every hps

--- hps/warmup_fixed_hps.py
+++ hps/warmup_fixed_hps.py
@@ -33,158 +33,158 @@ HAN = 'HAN'
 
 FIXED_HPS = {(B,): {
                     T: {
-                       TabNetHP.BATCH_SIZE: 15,
-                       TabNetHP.BETA: 0.10656,
-                       TabNetHP.GAMMA: 1.978489,
-                       TabNetHP.N_A: 3,
-                       TabNetHP.N_D: 7,
-                       TabNetHP.N_STEPS: 5,
-                       TabNetHP.LR: 0.05207
+                       TabNetHP.BATCH_SIZE.name: 15,
+                       TabNetHP.BETA.name: 0.10656,
+                       TabNetHP.GAMMA.name: 1.978489,
+                       TabNetHP.N_A.name: 3,
+                       TabNetHP.N_D.name: 7,
+                       TabNetHP.N_STEPS.name: 5,
+                       TabNetHP.LR.name: 0.05207
                     },
                     RF: {
-                        RandomForestHP.MAX_LEAF_NODES: 25,
-                        RandomForestHP.MAX_FEATURES: "sqrt",
-                        RandomForestHP.MAX_SAMPLES: 0.922954,
-                        RandomForestHP.MIN_SAMPLES_SPLIT: 4,
-                        RandomForestHP.N_ESTIMATORS: 1250
+                        RandomForestHP.MAX_LEAF_NODES.name: 25,
+                        RandomForestHP.MAX_FEATURES.name: "sqrt",
+                        RandomForestHP.MAX_SAMPLES.name: 0.922954,
+                        RandomForestHP.MIN_SAMPLES_SPLIT.name: 4,
+                        RandomForestHP.N_ESTIMATORS.name: 1250
                     },
                     XG: {
-                        XGBoostHP.ALPHA: 1.240812,
-                        XGBoostHP.BETA: 0.968769,
-                        XGBoostHP.LR: 0.250774,
-                        XGBoostHP.MAX_DEPTH: 1,
-                        XGBoostHP.SUBSAMPLE: 1,
+                        XGBoostHP.ALPHA.name: 1.240812,
+                        XGBoostHP.BETA.name: 0.968769,
+                        XGBoostHP.LR.name: 0.250774,
+                        XGBoostHP.MAX_DEPTH.name: 1,
+                        XGBoostHP.SUBSAMPLE.name: 1,
                     },
                     MLP: {
-                        MLPHP.ACTIVATION: "PReLU",
-                        MLPHP.ALPHA: 0.672994,
-                        MLPHP.BATCH_SIZE: 55,
-                        MLPHP.BETA: 0.729828,
-                        MLPHP.LR: 0.027521,
-                        MLPHP.N_LAYER: 1,
-                        MLPHP.N_UNIT: 5,
-                        MLPHP.RHO: 0.72377
+                        MLPHP.ACTIVATION.name: "PReLU",
+                        MLPHP.ALPHA.name: 0.672994,
+                        MLPHP.BATCH_SIZE.name: 55,
+                        MLPHP.BETA.name: 0.729828,
+                        MLPHP.LR.name: 0.027521,
+                        MLPHP.N_LAYER.name: 1,
+                        MLPHP.N_UNIT.name: 5,
+                        MLPHP.RHO.name: 0.72377
                     },
                     E: {
-                        MLPHP.ACTIVATION: "PReLU",
-                        MLPHP.ALPHA: 2.598665,
-                        MLPHP.BATCH_SIZE: 35,
-                        MLPHP.BETA: 2.759232,
-                        MLPHP.LR: 0.462388,
-                        MLPHP.N_LAYER: 0,
-                        MLPHP.N_UNIT: 5,
-                        MLPHP.RHO: 0.73456
+                        MLPHP.ACTIVATION.name: "PReLU",
+                        MLPHP.ALPHA.name: 2.598665,
+                        MLPHP.BATCH_SIZE.name: 35,
+                        MLPHP.BETA.name: 2.759232,
+                        MLPHP.LR.name: 0.462388,
+                        MLPHP.N_LAYER.name: 0,
+                        MLPHP.N_UNIT.name: 5,
+                        MLPHP.RHO.name: 0.73456
 
                     },
                    },
              (B, S): {
                       T: {
-                         TabNetHP.BATCH_SIZE: 15,
-                         TabNetHP.BETA: 0.07302,
-                         TabNetHP.GAMMA: 1.78896,
-                         TabNetHP.N_A: 6,
-                         TabNetHP.N_D: 6,
-                         TabNetHP.N_STEPS: 5,
-                         TabNetHP.LR: 0.02654
+                         TabNetHP.BATCH_SIZE.name: 15,
+                         TabNetHP.BETA.name: 0.07302,
+                         TabNetHP.GAMMA.name: 1.78896,
+                         TabNetHP.N_A.name: 6,
+                         TabNetHP.N_D.name: 6,
+                         TabNetHP.N_STEPS.name: 5,
+                         TabNetHP.LR.name: 0.02654
                       },
                       RF: {
-                         RandomForestHP.MAX_LEAF_NODES: 25,
-                         RandomForestHP.MAX_FEATURES: "log2",
-                         RandomForestHP.MAX_SAMPLES: 0.968991,
-                         RandomForestHP.MIN_SAMPLES_SPLIT: 3,
-                         RandomForestHP.N_ESTIMATORS: 2000
+                         RandomForestHP.MAX_LEAF_NODES.name: 25,
+                         RandomForestHP.MAX_FEATURES.name: "log2",
+                         RandomForestHP.MAX_SAMPLES.name: 0.968991,
+                         RandomForestHP.MIN_SAMPLES_SPLIT.name: 3,
+                         RandomForestHP.N_ESTIMATORS.name: 2000
                       },
                       XG: {
-                          XGBoostHP.ALPHA: 1.821231,
-                          XGBoostHP.BETA: 1.714922,
-                          XGBoostHP.LR: 0.116989,
-                          XGBoostHP.MAX_DEPTH: 1,
-                          XGBoostHP.SUBSAMPLE: 1
+                          XGBoostHP.ALPHA.name: 1.821231,
+                          XGBoostHP.BETA.name: 1.714922,
+                          XGBoostHP.LR.name: 0.116989,
+                          XGBoostHP.MAX_DEPTH.name: 1,
+                          XGBoostHP.SUBSAMPLE.name: 1
 
                       },
                       MLP: {
-                          MLPHP.ACTIVATION: "PReLU",
-                          MLPHP.ALPHA: 1.603513,
-                          MLPHP.BATCH_SIZE: 15,
-                          MLPHP.BETA: 1.88753,
-                          MLPHP.LR: 0.143939,
-                          MLPHP.N_LAYER: 1,
-                          MLPHP.N_UNIT: 2,
-                          MLPHP.RHO: 0.868263
+                          MLPHP.ACTIVATION.name: "PReLU",
+                          MLPHP.ALPHA.name: 1.603513,
+                          MLPHP.BATCH_SIZE.name: 15,
+                          MLPHP.BETA.name: 1.88753,
+                          MLPHP.LR.name: 0.143939,
+                          MLPHP.N_LAYER.name: 1,
+                          MLPHP.N_UNIT.name: 2,
+                          MLPHP.RHO.name: 0.868263
                       },
                       E: {
-                          MLPHP.ACTIVATION: "PReLU",
-                          MLPHP.ALPHA: 0.962996,
-                          MLPHP.BATCH_SIZE: 45,
-                          MLPHP.BETA: 2.030289,
-                          MLPHP.LR: 0.056097,
-                          MLPHP.N_LAYER: 0,
-                          MLPHP.N_UNIT: 5,
-                          MLPHP.RHO: 0.220021
+                          MLPHP.ACTIVATION.name: "PReLU",
+                          MLPHP.ALPHA.name: 0.962996,
+                          MLPHP.BATCH_SIZE.name: 45,
+                          MLPHP.BETA.name: 2.030289,
+                          MLPHP.LR.name: 0.056097,
+                          MLPHP.N_LAYER.name: 0,
+                          MLPHP.N_UNIT.name: 5,
+                          MLPHP.RHO.name: 0.220021
                       },
                       HAN: {
-                          HanHP.ALPHA: 0.558703,
-                          HanHP.BATCH_SIZE: 15,
-                          HanHP.BETA: 1.520712,
-                          HanHP.HIDDEN_SIZE: 4,
-                          HanHP.LR: 0.109538,
-                          HanHP.NUM_HEADS: 19,
-                          HanHP.RHO: 1.291954
+                          HanHP.ALPHA.name: 0.558703,
+                          HanHP.BATCH_SIZE.name: 15,
+                          HanHP.BETA.name: 1.520712,
+                          HanHP.HIDDEN_SIZE.name: 4,
+                          HanHP.LR.name: 0.109538,
+                          HanHP.NUM_HEADS.name: 19,
+                          HanHP.RHO.name: 1.291954
                       }
                      },
              (B, S, G): {
                          T: {
-                             TabNetHP.BATCH_SIZE: 15,
-                             TabNetHP.BETA: 0.084007,
-                             TabNetHP.GAMMA: 1.684942,
-                             TabNetHP.N_A: 5,
-                             TabNetHP.N_D: 2,
-                             TabNetHP.N_STEPS: 4,
-                             TabNetHP.LR: 0.011037
+                             TabNetHP.BATCH_SIZE.name: 15,
+                             TabNetHP.BETA.name: 0.084007,
+                             TabNetHP.GAMMA.name: 1.684942,
+                             TabNetHP.N_A.name: 5,
+                             TabNetHP.N_D.name: 2,
+                             TabNetHP.N_STEPS.name: 4,
+                             TabNetHP.LR.name: 0.011037
                          },
                          RF: {
-                             RandomForestHP.MAX_LEAF_NODES: 25,
-                             RandomForestHP.MAX_FEATURES: "sqrt",
-                             RandomForestHP.MAX_SAMPLES: 0.994949,
-                             RandomForestHP.MIN_SAMPLES_SPLIT: 2,
-                             RandomForestHP.N_ESTIMATORS: 1250
+                             RandomForestHP.MAX_LEAF_NODES.name: 25,
+                             RandomForestHP.MAX_FEATURES.name: "sqrt",
+                             RandomForestHP.MAX_SAMPLES.name: 0.994949,
+                             RandomForestHP.MIN_SAMPLES_SPLIT.name: 2,
+                             RandomForestHP.N_ESTIMATORS.name: 1250
                          },
                          XG: {
-                             XGBoostHP.ALPHA: 1.883789,
-                             XGBoostHP.BETA: 1.757092,
-                             XGBoostHP.LR: 0.186442,
-                             XGBoostHP.MAX_DEPTH: 1,
-                             XGBoostHP.SUBSAMPLE: 1
+                             XGBoostHP.ALPHA.name: 1.883789,
+                             XGBoostHP.BETA.name: 1.757092,
+                             XGBoostHP.LR.name: 0.186442,
+                             XGBoostHP.MAX_DEPTH.name: 1,
+                             XGBoostHP.SUBSAMPLE.name: 1
                          },
                          MLP: {
-                             MLPHP.ACTIVATION: "PReLU",
-                             MLPHP.ALPHA: 1.409467,
-                             MLPHP.BATCH_SIZE: 25,
-                             MLPHP.BETA: 0.449466,
-                             MLPHP.LR: 0.006111,
-                             MLPHP.N_LAYER: 5,
-                             MLPHP.N_UNIT: 3,
-                             MLPHP.RHO: 0.87755
+                             MLPHP.ACTIVATION.name: "PReLU",
+                             MLPHP.ALPHA.name: 1.409467,
+                             MLPHP.BATCH_SIZE.name: 25,
+                             MLPHP.BETA.name: 0.449466,
+                             MLPHP.LR.name: 0.006111,
+                             MLPHP.N_LAYER.name: 5,
+                             MLPHP.N_UNIT.name: 3,
+                             MLPHP.RHO.name: 0.87755
                          },
                          E: {
-                             MLPHP.ACTIVATION: "PReLU",
-                             MLPHP.ALPHA: 1.788895,
-                             MLPHP.BATCH_SIZE: 45,
-                             MLPHP.BETA: 1.786256,
-                             MLPHP.LR: 0.05359,
-                             MLPHP.N_LAYER: 0,
-                             MLPHP.N_UNIT: 5,
-                             MLPHP.RHO: 0.862334
+                             MLPHP.ACTIVATION.name: "PReLU",
+                             MLPHP.ALPHA.name: 1.788895,
+                             MLPHP.BATCH_SIZE.name: 45,
+                             MLPHP.BETA.name: 1.786256,
+                             MLPHP.LR.name: 0.05359,
+                             MLPHP.N_LAYER.name: 0,
+                             MLPHP.N_UNIT.name: 5,
+                             MLPHP.RHO.name: 0.862334
 
                          },
                          HAN: {
-                             HanHP.ALPHA: 1.305886,
-                             HanHP.BATCH_SIZE: 15,
-                             HanHP.BETA: 1.18774,
-                             HanHP.HIDDEN_SIZE: 10,
-                             HanHP.LR: 0.084286,
-                             HanHP.NUM_HEADS: 12,
-                             HanHP.RHO: 0.545613
+                             HanHP.ALPHA.name: 1.305886,
+                             HanHP.BATCH_SIZE.name: 15,
+                             HanHP.BETA.name: 1.18774,
+                             HanHP.HIDDEN_SIZE.name: 10,
+                             HanHP.LR.name: 0.084286,
+                             HanHP.NUM_HEADS.name: 12,
+                             HanHP.RHO.name: 0.545613
                          }
                         }
              }

7a3203ed0bfc7fe0fa3ce52a00142805862b2795
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-01-25 12:06:41 -0500

    New GeneEncoder abstract class

--- src/models/blocks/genes_signature_block.py
+++ src/models/blocks/genes_signature_block.py
@@ -9,6 +9,7 @@ Description: Defines the modules in charge of encoding
 Date of last modification: 2022/01/19
 """
 
+from abc import ABC
 from src.models.abstract_models.encoder import Encoder
 from src.models.blocks.mlp_blocks import BaseBlock, EntityEmbeddingBlock
 from torch import bmm, einsum, exp, normal, tensor, zeros
@@ -17,7 +18,7 @@ from torch.nn.functional import leaky_relu, relu
 from typing import Dict, List
 
 
-class GeneGraphEncoder(Encoder, Module):
+class GeneEncoder(ABC, Encoder, Module):
     """
     Generates a signature (embedding) associated to an individual genes graph
     """
@@ -27,8 +28,112 @@ class GeneGraphEncoder(Encoder, Module):
                  signature_size: int = 10,
                  genes_emb_sharing: bool = False):
         """
-        Builds the entity embedding block, the convolutional layer, the linear layer,
-        the batch norm and sets other protected attributes using the Encoder constructor
+        Saves protected attributes and builds the entity embedding block
+
+        Args:
+            gene_idx_groups: dictionary where keys are names of chromosomes and values
+                             are list of idx referring to columns of genes associated to
+                             the chromosome
+            hidden_size: embedding size of each genes during intermediate
+                         signature creation procedure
+            signature_size: final genomic signature size (output size)
+            genes_emb_sharing: If True, genes will share the same entity embedding layer
+
+        Returns: None
+        """
+        # We extract the genes idx
+        self._gene_idx_groups = gene_idx_groups
+        self._genes_idx = []
+        for idx in self._gene_idx_groups.values():
+            self._genes_idx.extend(idx)
+
+        # We save the nb of genes, the hidden size and the nb of chromosomes
+        self._hidden_size = hidden_size
+        self._nb_genes = len(self._genes_idx)
+        self._nb_chrom = len(self._gene_idx_groups.keys())
+
+        # Setting of input and output sizes protected attributes
+        Module.__init__(self)
+        Encoder.__init__(self,
+                         input_size=self._nb_genes,
+                         output_size=signature_size)
+
+        # Creation of entity embedding block
+        self._entity_emb_block = EntityEmbeddingBlock(cat_sizes=[3]*self._nb_genes,
+                                                      cat_emb_sizes=[self._hidden_size]*self._nb_genes,
+                                                      cat_idx=self._genes_idx,
+                                                      embedding_sharing=genes_emb_sharing)
+
+        # Creation of a cache to store gene embeddings
+        self._embedding_cache = None
+
+    @property
+    def cache(self) -> tensor:
+        return self._embedding_cache
+
+    @property
+    def gene_idx_groups(self) -> Dict[str, List[int]]:
+        return self._gene_idx_groups
+
+    @property
+    def hidden_size(self) -> int:
+        return self._hidden_size
+
+    def _builds_chrom_composition_mat(self) -> tensor:
+        """
+        Builds a (NB_CHROM, NB_GENES) tensor where each element at the position
+        i,j is a 1 if gene-j is part of chromosome-i and 0 otherwise
+
+        Returns: (NB_CHROM, NB_GENES) tensor
+        """
+        # We save coordinates with values in the sparse matrix
+        x_coords, y_coords = [], []
+        y = 0
+        for x, idx in enumerate(self._gene_idx_groups.values()):
+            nb_genes_in_chrom = len(idx)
+            x_coords += [x]*nb_genes_in_chrom
+            y_coords += range(y, y + nb_genes_in_chrom)
+            y += nb_genes_in_chrom
+
+        # We set the sparse matrix
+        mat = zeros(self.__nb_chrom, self.__nb_genes, requires_grad=False)
+        mat[x_coords, y_coords] = 1
+
+        return mat
+
+    def _compute_genes_emb(self, x: tensor) -> tensor:
+        """
+        Extracts genes embeddings using the entity embedding block
+        and saves them in a cache
+
+        Args:
+            x: (N, D) tensor with D-dimensional samples
+
+        Returns: (N, NB_GENES, HIDDEN_SIZE) tensor
+        """
+        # Entity embedding on genes
+        h = self._entity_emb_block(x)  # (N, D) -> (N, HIDDEN_SIZE*NB_GENES)
+
+        # Resize embeddings
+        h = h.reshape(h.shape[0], self._nb_genes, self._hidden_size)  # (N, NB_GENES, HIDDEN_SIZE)
+        self._embedding_cache = h
+
+        return h
+
+
+class GeneGraphEncoder(GeneEncoder):
+    """
+    Generates a signature (embedding) associated to an individual genes graph
+    with average of gene embeddings followed by a convolutional and a linear layer
+    """
+    def __init__(self,
+                 gene_idx_groups: Dict[str, List[int]],
+                 hidden_size: int = 3,
+                 signature_size: int = 10,
+                 genes_emb_sharing: bool = False):
+        """
+        Sets protected attributes with parent's constructor, the convolutional layer,
+        the linear layer, the batch norm and sets other protected attributes using the Encoder constructor
 
         Args:
             gene_idx_groups: dictionary where keys are names of chromosomes and values
@@ -39,89 +144,32 @@ class GeneGraphEncoder(Encoder, Module):
             signature_size: final genomic signature size (output size)
             genes_emb_sharing: If True, genes will share the same entity embedding layer
         """
-        # We extract the genes idx
-        self.__gene_idx_groups = gene_idx_groups
-        self.__genes_idx = []
-        for idx in self.__gene_idx_groups.values():
-            self.__genes_idx.extend(idx)
-
-        # We save the nb of genes, the hidden size and the nb of chromosomes
-        self.__hidden_size = hidden_size
-        self.__nb_genes = len(self.__genes_idx)
-        self.__nb_chrom = len(self.__gene_idx_groups.keys())
-
-        # Setting of input and output sizes protected attributes
-        Module.__init__(self)
-        Encoder.__init__(self,
-                         input_size=self.__nb_genes,
-                         output_size=signature_size)
-
-        # Creation of entity embedding block
-        self._entity_emb_block = EntityEmbeddingBlock(cat_sizes=[3]*self.__nb_genes,
-                                                      cat_emb_sizes=[self.__hidden_size]*self.__nb_genes,
-                                                      cat_idx=self.__genes_idx,
-                                                      embedding_sharing=genes_emb_sharing)
+        # Call of parent's constructor
+        super().__init__(gene_idx_groups=gene_idx_groups,
+                         hidden_size=hidden_size,
+                         signature_size=signature_size,
+                         genes_emb_sharing=genes_emb_sharing)
 
         # Creation of the matrix used to calculate the average of entity embeddings
         # within each chromosome. This matrix will not be updated
-        self.__chrom_weight_mat = zeros(self.__nb_chrom, self.__nb_genes, requires_grad=False)
-        self.__set_chromosome_weight_mat(self.__gene_idx_groups)
+        self.__chrom_weight_mat = self._builds_chrom_composition_mat()
+        self.__chrom_weight_mat /= self.__chrom_weight_mat.sum(dim=1).reshape(-1, 1)
 
         # Convolutional layer that must be applied to each chromosome embedding
         self._conv_layer = Conv1d(in_channels=1,
                                   out_channels=1,
-                                  kernel_size=(self.__hidden_size,),
-                                  stride=(self.__hidden_size,))
+                                  kernel_size=(self._hidden_size,),
+                                  stride=(self._hidden_size,))
 
         # Linear layer that gives the final signature
-        self._linear_layer = Linear(self.__nb_chrom, signature_size)
+        self._linear_layer = Linear(self._nb_chrom, signature_size)
 
         # Batch norm layer that normalize final signatures
         self._bn = BatchNorm1d(signature_size)
 
-        # Creation of a cache needed to calculate loss
-        self.__embedding_cache = None
-
-    @property
-    def cache(self) -> tensor:
-        return self.__embedding_cache
-
     @property
     def chrom_weight_mat(self) -> tensor:
         return self.__chrom_weight_mat
-
-    @property
-    def gene_idx_groups(self) -> Dict[str, List[int]]:
-        return self.__gene_idx_groups
-
-    @property
-    def hidden_size(self) -> int:
-        return self.__hidden_size
-
-    def __set_chromosome_weight_mat(self, gene_idx_groups: Dict[str, List[int]]) -> None:
-        """
-        Sets the matrix used to calculate mean of entity embeddings within each chromosome
-
-        Args:
-            gene_idx_groups: dictionary where keys are names of chromosomes and values
-                             are list of idx referring to columns of genes associated to
-                             the chromosome
-
-        Returns: None
-        """
-        # We save coordinates with values in the sparse matrix
-        x_coords, y_coords = [], []
-        y = 0
-        for x, idx in enumerate(gene_idx_groups.values()):
-            nb_genes_in_chrom = len(idx)
-            x_coords += [x]*nb_genes_in_chrom
-            y_coords += range(y, y + nb_genes_in_chrom)
-            y += nb_genes_in_chrom
-
-        # We set the sparse matrix with weights to calculate averages
-        # for each chromosome graph
-        self.__chrom_weight_mat[x_coords, y_coords] = 1
-        self.__chrom_weight_mat /= self.__chrom_weight_mat.sum(dim=1).reshape(-1, 1)
 
     def forward(self, x: tensor) -> tensor:
         """
@@ -139,18 +187,15 @@ class GeneGraphEncoder(Encoder, Module):
         Returns: (N, D') tensor where D' is the signature size
         """
         # Entity embedding on genes
-        h = self._entity_emb_block(x)  # (N, D) -> (N, HIDDEN_SIZE*NB_GENES)
-
-        # Resize embeddings
-        self.__embedding_cache = h.reshape(h.shape[0], self.__nb_genes, self.__hidden_size)  # (N, NB_GENES, HIDDEN_SIZE)
+        h = self._compute_genes_emb(x)  # (N, D) -> (N, NB_GENES, HIDDEN_SIZE)
 
         # Compute entity embedding averages per chromosome subgraphs for each individual
-        # (NB_CHROM, NB_GENES)x(N, NB_GENES, HIDDEN_SIZE) -> (N, NB_CHROM, HIDDEN_SIZE)
-        h = einsum('ij,kjf->kif', self.__chrom_weight_mat, self.__embedding_cache)
+        # (NB_CHROM, NB_GENES)(N, NB_GENES, HIDDEN_SIZE) -> (N, NB_CHROM, HIDDEN_SIZE)
+        h = einsum('ij,kjf->kif', self.__chrom_weight_mat, h)
 
         # Concatenate all chromosome averages side to side
         # (N, NB_CHROM, HIDDEN_SIZE) -> (N, NB_CHROM*HIDDEN_SIZE)
-        h = h.reshape(h.shape[0], self.__nb_chrom*self.__hidden_size)
+        h = h.reshape(h.shape[0], self._nb_chrom*self._hidden_size)
 
         # Add a dummy dimension
         h.unsqueeze_(dim=1)
@@ -232,7 +277,7 @@ class GeneSignatureDecoder(Module):
 
         # Multiplication by gene_weight_mat to recover gene embeddings
         # the mask ensure that 0's are not updated in the gene_weight_mat
-        # (NB_GENES, NB_CHROM)x(N, NB_CHROM, HIDDEN_SIZE) -> (N, NB_GENES, HIDDEN_SIZE)
+        # (NB_GENES, NB_CHROM)(N, NB_CHROM, HIDDEN_SIZE) -> (N, NB_GENES, HIDDEN_SIZE)
         h = einsum('ij,kjf->kif', (self.__gene_weight_mat*self.__mask), h)
 
         return h
@@ -267,7 +312,7 @@ class GeneAttentionLayer(Module):
         Saves the chrom_composition_mat and initializes the attention matrix
         Args:
             chrom_composition_mat: (NB_CHROM, NB_GENES) tensor where each element at the position
-                                   i,j is a 1 if gene-j is part of chromosome-i  and 0 otherwise
+                                   i,j is a 1 if gene-j is part of chromosome-i and 0 otherwise
             hidden_size: size of gene embeddings
 
         Returns: None

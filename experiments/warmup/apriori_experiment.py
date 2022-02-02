1e7b0d2b032345a02ffbbf80ad79308559232937
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-02-02 14:42:31 -0500

    experiment file updated with functions in new emplacement

--- experiments/warmup/apriori_experiment.py
+++ experiments/warmup/apriori_experiment.py
@@ -13,115 +13,8 @@ from apyori import apriori
 from os import mkdir
 from os.path import dirname, exists, realpath, join
 from time import time
-from typing import Dict, List, Union, Any
 
-import argparse
-import json
 import sys
-
-
-def argument_parser():
-    """
-    This function defines a parser that enables user to easily run different experiments
-    """
-    # Create a parser
-    parser = argparse.ArgumentParser(usage='\n python apriori_experiment.py',
-                                     description="Runs the apriori algorithm on the different split of the"
-                                                 "warmup dataset")
-
-    # Nb inner split and nb outer split selection
-    parser.add_argument('-min_sup', '--min_support', type=float, default=0.1,
-                        help='Minimal support value (default = 0.1)')
-    parser.add_argument('-min_conf', '--min_confidence', type=float, default=0.60,
-                        help='Minimal confidence value (default = 0.60)')
-    parser.add_argument('-min_lift', '--min_lift', type=float, default=1.20,
-                        help='Minimal lift value (default = 1.20)')
-    parser.add_argument('-max_length', '--max_length', type=int, default=1,
-                        help='Max cardinality of item sets at the left side of rules (default = 1)')
-    parser.add_argument('-nb_groups', '--nb_groups', type=int, default=2,
-                        help='Number quantiles considered to create V02 groups (default = 2)')
-
-    arguments = parser.parse_args()
-
-    # Print arguments
-    print("\nThe inputs are:")
-    for arg in vars(arguments):
-        print("{}: {}".format(arg, getattr(arguments, arg)))
-    print("\n")
-
-    return arguments
-
-
-def print_and_save_rules(rules: List[Any],
-                         settings: Dict[str, Union[float, int]],
-                         folder_path: str,
-                         json_filename: str,
-                         start_time: Any,
-                         save_genes: bool = False) -> None:
-    """
-    Prints and saves the rules found in a json file
-
-    Args:
-        rules: list of rules found with apriori
-        settings: dictionary of apriori settings
-        folder_path: path of the folder used to store json file with results
-        json_filename: name of the json file used to store the results
-        start_time: experiment start time
-        save_genes: True if we want to save genes involved in rules
-
-    Returns: None
-    """
-    rules_dictionary = {'Settings': settings, 'Rules': {}}
-
-    for item in rules:
-
-        # We print rule
-        rule = f"{list(item.ordered_statistics[0].items_base)} -> {list(item.ordered_statistics[0].items_add)}"
-        print(f"Rule : {rule}")
-
-        # We print support
-        support = item[1]
-        print(f"Support: {support}")
-
-        # We print confidence and lift
-        confidence = item[2][0][2]
-        lift = item[2][0][3]
-        print(f"Confidence: {confidence}")
-        print(f"Lift: {lift}")
-
-        # We save statistics in the dictionary
-        rules_dictionary['Rules'][rule] = {'Support': support, 'Lift': lift, 'Confidence': confidence}
-        print("="*40)
-
-    if save_genes:
-
-        # We initialize a genes counter
-        genes_list = []
-
-        for item in rules:
-            for chrom_pos_expression in list(item.ordered_statistics[0].items_base):
-
-                # We extract chrom pos and save it
-                chrom_pos_splits = chrom_pos_expression.split("_")
-                chrom_pos = f"{chrom_pos_splits[0]}_{chrom_pos_splits[1]}"
-
-                if chrom_pos_splits[-1] in ['0/0', '1/1', '0/1']:
-                    genes_list.append(chrom_pos)
-
-        rules_dictionary['Genes'] = list(set(genes_list))
-
-    # We save and print the time taken
-    time_taken = round((time() - start_time) / 60, 2)
-    rules_dictionary['Settings']['time'] = time_taken
-    print("Time Taken (minutes): ", time_taken)
-
-    # We save the number of rules
-    rules_dictionary['Settings']['nb_of_rules'] = len(rules)
-
-    # We save the dictionary in a json file
-    filepath = join(folder_path, f"{json_filename}.json")
-    with open(filepath, "w") as file:
-        json.dump(rules_dictionary, file, indent=True)
 
 
 if __name__ == '__main__':
@@ -133,10 +26,11 @@ if __name__ == '__main__':
     from src.data.processing.datasets import MaskType
     from src.data.processing.sampling import extract_masks, GeneChoice, get_warmup_data
     from src.data.processing.preprocessing import preprocess_for_apriori
-    from src.utils.results_analysis import get_apriori_statistics
+    from src.utils.argparsers import apriori_argparser
+    from src.utils.results_analysis import get_apriori_statistics, print_and_save_apriori_rules
 
     # Arguments parsing
-    args = argument_parser()
+    args = apriori_argparser()
 
     # We save start time
     start = time()
@@ -186,7 +80,7 @@ if __name__ == '__main__':
                     'min_lift': args.min_lift, 'max_length': args.max_length, 'nb_VO2_groups': args.nb_groups}
 
         # We print and save all the rules
-        print_and_save_rules(association_results, settings, f1, f'warmup_apriori_{i}', start)
+        print_and_save_apriori_rules(association_results, settings, f1, f'warmup_apriori_{i}', start)
 
         # We print and save the rules only related to VO2
         temp_list = []
@@ -198,7 +92,7 @@ if __name__ == '__main__':
 
         association_results = temp_list
 
-        print_and_save_rules(association_results, settings, f2, f'warmup_apriori_vo2_{i}', start, save_genes=True)
+        print_and_save_apriori_rules(association_results, settings, f2, f'warmup_apriori_vo2_{i}', start, save_genes=True)
 
     # We compute summary of apriori results for rules associated to V02
     get_apriori_statistics(f2)

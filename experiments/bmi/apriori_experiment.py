c8e09476a87b9d2c0faa2c73bd31b3b1276416da
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-02-08 09:30:10 -0500

    Experiment modified to include the usage of a function

--- experiments/bmi/apriori_experiment.py
+++ experiments/bmi/apriori_experiment.py
@@ -9,8 +9,6 @@ Description: This file is used to identify meaningful association
 Date of last modification : 2022/02/02
 """
 
-from apyori import apriori
-from os import mkdir
 from os.path import dirname, exists, realpath, join
 from time import time
 
@@ -23,11 +21,9 @@ if __name__ == '__main__':
     sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
     from settings.paths import Paths
     from src.data.extraction.data_management import PetaleDataManager
-    from src.data.processing.datasets import MaskType
     from src.data.processing.sampling import extract_masks, GeneChoice, get_learning_one_data
-    from src.data.processing.preprocessing import preprocess_for_apriori
     from src.utils.argparsers import apriori_argparser
-    from src.utils.results_analysis import get_apriori_statistics, print_and_save_apriori_rules
+    from src.utils.experiments import run_apriori_experiment
 
     # Arguments parsing
     args = apriori_argparser()
@@ -42,57 +38,11 @@ if __name__ == '__main__':
     # Extraction of masks
     masks = extract_masks(Paths.BMI_MASK, k=10, l=0)
 
-    # We only keep categorical columns and targets
-    df = df[cat_cols + [target]]
-
-    # We save folder names for different results
-    f1, f2 = join(Paths.EXPERIMENTS_RECORDS, "L1_apriori"), join(Paths.EXPERIMENTS_RECORDS, "L1_apriori_BMI")
-    for f in [f1, f2]:
-        if not exists(f):
-            mkdir(f)
-
-    for i in range(len(masks.keys())):
-
-        df_subset = df.iloc[masks[i][MaskType.TRAIN]]
-
-        # We preprocess data
-        records = preprocess_for_apriori(df_subset, cont_cols={target: args.nb_groups}, cat_cols=cat_cols)
-
-        # We print the number of itemsets
-        print(f"Number of records : {len(records)}")
-
-        # We run apriori algorithm
-        association_rules = apriori(records, min_support=args.min_support, min_confidence=args.min_confidence,
-                                    min_lift=args.min_lift, max_length=(args.max_length + 1))
-        association_results = list(association_rules)
-
-        # We print the number of rules
-        print(f"Number of rules : {len(association_results)}")
-
-        # We clean results to only keep association rules of type (... -> BMI)
-        association_results = [rule for rule in association_results if len(list(rule.ordered_statistics[0].items_add)) < 2]
-
-        # We sort the rules by lift
-        association_results = sorted(association_results, key=lambda x: x[2][0][3], reverse=True)
-
-        # We save a dictionary with apriori settings
-        settings = {'min_support': args.min_support, 'min_confidence': args.min_confidence,
-                    'min_lift': args.min_lift, 'max_length': args.max_length, 'nb_BMI_groups': args.nb_groups}
-
-        # We print and save all the rules
-        print_and_save_apriori_rules(association_results, settings, f1, f'L1_apriori_{i}', start)
-
-        # We print and save the rules only related to BM1
-        temp_list = []
-        for rule in association_results:
-            right_part = list(rule.ordered_statistics[0].items_add)
-            right_part = right_part[0]
-            if right_part.split(" <")[0] == target.upper() or right_part.split(" >")[0] == target.upper():
-                temp_list.append(rule)
-
-        association_results = temp_list
-
-        print_and_save_apriori_rules(association_results, settings, f2, f'L1_apriori_BMI_{i}', start, save_genes=True)
-
-    # We compute summary of apriori results for rules associated to BMI
-    get_apriori_statistics(f2)
+    # We run apriori experiment
+    run_apriori_experiment(experiment_name='L1',
+                           df=df,
+                           target=target,
+                           cat_cols=cat_cols,
+                           masks=masks,
+                           arguments=args,
+                           continuous_target=True)

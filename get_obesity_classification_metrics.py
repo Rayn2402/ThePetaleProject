3606db82359075ac8a96bae18e41ad266df6f83f
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-03-23 15:42:02 -0400

    Usage of new constants

--- get_obesity_classification_metrics.py
+++ get_obesity_classification_metrics.py
@@ -8,26 +8,24 @@ Description: Adds the sensitivity, specificity and bAcc scores to obesity experi
 """
 from pandas import DataFrame
 from src.data.extraction.constants import AGE, OBESITY, OBESITY_PERCENTILE, OBESITY_TARGET, SEX, TOTAL_BODY_FAT
-from src.utils.results_analysis import get_classification_metrics
+from src.utils.results_analysis import CLASS_PRED, get_classification_metrics, REG_PRED
 
 if __name__ == '__main__':
 
     # Class generation function
-    def get_class_labels(df: DataFrame,
-                         pred_col: str) -> DataFrame:
+    def get_class_labels(df: DataFrame) -> DataFrame:
         """
         Generates class labels from real valued predictions
 
         Args:
             df: df with predictions
-            pred_col: name of the column that will store the class predictions
 
         Returns: df
 
         """
-        df.loc[(df[SEX] == 'Women') & (df[AGE] > 18) & (df[TOTAL_BODY_FAT] > 35), [pred_col]] = 1
-        df.loc[(df[SEX] == 'Men') & (df[AGE] > 18) & (df[TOTAL_BODY_FAT] > 25), [pred_col]] = 1
-        df.loc[(df[AGE] < 18) & (df[TOTAL_BODY_FAT] >= OBESITY_PERCENTILE), [pred_col]] = 1
+        df.loc[(df[SEX] == 'Women') & (df[AGE] > 18) & (df[REG_PRED] > 35), [CLASS_PRED]] = 1
+        df.loc[(df[SEX] == 'Men') & (df[AGE] > 18) & (df[REG_PRED] > 25), [CLASS_PRED]] = 1
+        df.loc[(df[AGE] < 18) & (df[REG_PRED] >= OBESITY_PERCENTILE), [CLASS_PRED]] = 1
 
         return df
 

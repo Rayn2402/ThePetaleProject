cb1ee876da53db3481a67d87e58acac4f86b7a07
Rayn2402 <rayn2402@usherbrooke.ca>
2022-03-30 10:57:58 -0400

    New function + feature importance plot rotated

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -181,17 +181,58 @@ def visualize_importance(data: Dict[str, Dict[str, float]],
         stds.append(data[key][STD])
         labels.append(key)
 
-    # We sort the list according values
+    # We sort the list according to their values
     sorted_means = sorted(means)
     sorted_labels = sorted(labels, key=lambda x: means[labels.index(x)])
     sorted_stds = sorted(stds, key=lambda x: means[stds.index(x)])
 
     # We build the plot
-    x_pos = arange(len(labels))
-    fig, ax = plt.subplots(figsize=(10, 7))
-    ax.bar(x_pos, sorted_means, yerr=sorted_stds, capsize=5)
-    ax.set_xticks(x_pos)
-    ax.set_xticklabels(sorted_labels)
+    y_pos = arange(len(labels))
+    fig, ax = plt.subplots(figsize=(15, 7))
+    ax.barh(y_pos, sorted_means, xerr=sorted_stds, capsize=5)
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(sorted_labels)
+    ax.set_xlabel('Importance')
+    ax.set_title(figure_title)
+
+    # We save the plot
+    plt.savefig(filename)
+    plt.close()
+
+
+def visualize_scaled_importance(data: Dict[str, Dict[str, float]],
+                                figure_title: str,
+                                filename: str) -> None:
+    """
+    Creates a bar plot with mean and standard deviations
+    of variable importance contained within the dictionary
+
+    Args:
+        data: dictionary with variable name as keys and "mean" and "std" as values
+        figure_title: name appearing over the plot
+        filename: name of the file in which the figure is saved
+
+    Returns: None
+    """
+    # We initialize two lists for the scaled values and the labels
+    scaled_imp, labels = [], []
+
+    # We collect the data of each hyperparameter importance
+    for key in data.keys():
+        scaled_imp.append(data[key][MEAN]/data[key][STD])
+        labels.append(key)
+
+    # We sort the list according values
+    sorted_scaled_imp = sorted(scaled_imp)
+    sorted_labels = sorted(labels, key=lambda x: scaled_imp[labels.index(x)])
+
+    # We build the plot
+    y_pos = arange(len(labels))
+    fig, ax = plt.subplots(figsize=(15, 7))
+    ax.barh(y_pos, sorted_scaled_imp, capsize=5)
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(sorted_labels)
+    ax.set_xlabel('Importance')
     ax.set_title(figure_title)
 
     # We save the plot

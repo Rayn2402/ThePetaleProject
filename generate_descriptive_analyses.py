edf853ff59eaf79b59e45052a0e9df9cf5c4fee4
Rayn2402 <rayn2402@usherbrooke.ca>
2022-04-26 15:46:10 -0400

    script updated to match with return type of get_missing_data_count

--- generate_descriptive_analyses.py
+++ generate_descriptive_analyses.py
@@ -7,7 +7,7 @@ Author: Nicolas Raymond
 Description: This file stores the procedures that needs to be executed in order to extract descriptive tables
              with information from all variables of a table.
 
-Date of last modification: 2021/11/15
+Date of last modification: 2022/04/26
 """
 from src.data.extraction.data_management import PetaleDataManager
 
@@ -24,5 +24,6 @@ if __name__ == '__main__':
     print(stats, "\n\n")
 
     # We count the missing data
-    missing = data_manager.get_missing_data_count(TABLE)
-    print(missing)
+    df, summary_dict = data_manager.get_missing_data_count(TABLE)
+    print(df, "\n\n")
+    print(summary_dict)

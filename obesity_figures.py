bf4bdea30095d1da7015c1ecd8277c101a2c0fef
Rayn2402 <rayn2402@usherbrooke.ca>
2022-05-12 11:36:40 -0400

    script updated to generate a single plot

--- obesity_figures.py
+++ obesity_figures.py
@@ -22,29 +22,16 @@ if __name__ == '__main__':
     # Enable LaTeX
     plt.rc('text', usetex=True)
 
-    # We create the subplots
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4), sharey=True)
+    # Figure creation
+    plt.plot(df.index, df['target'], 'o', color='grey', label='Targets')
+    plt.plot(df.index, df['Enet'], 'o', label='Linear Reg. (B)')
+    plt.plot(df.index, df['GGAE'], 'o', label='Linear Reg. + GGAE (B + G)')
+    plt.xticks(rotation=90)
 
-    # We set the axes
-    axes[0].plot(df.index, df['target'], 'o', color='grey', label='Targets')
-    axes[0].plot(df.index, df['Enet'], 'o', label='Linear Reg. (B)')
-    axes[0].plot(df.index, df['GGAE'], 'o', label='Linear Reg. + GGAE (B + G)')
+    plt.ylabel('Total Body Fat (\\%)')
+    plt.xlabel('Participants')
+    plt.legend()
+    plt.tight_layout()
 
-    axes[1].plot(df.index, df['target'], 'o', color='grey')
-    axes[1].plot(df.index, df['XGBoost'], 'o', label='XGBoost (B + G)', color='green')
-
-    for ax in axes:
-        ax.xaxis.set_tick_params(rotation=90)
-
-    # We set the titles
-    fig.supylabel('Total Body Fat (\\%)')
-    fig.supxlabel('Participants', x=0.528)
-
-    # We set the labels
-    handles, labels = [(a + b) for a, b in zip(axes[0].get_legend_handles_labels(), axes[1].get_legend_handles_labels())]
-    fig.legend(handles, labels, bbox_to_anchor=(0.31, 0.96))
-
-    # Figure saving
-    fig.tight_layout()
     for f in ['pdf', 'svg']:
         plt.savefig(f'obesity_predictions.{f}')

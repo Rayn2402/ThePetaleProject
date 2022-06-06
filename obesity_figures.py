97c78a277e97b2bbad48d0b11103ec30beccb98a
Rayn2402 <rayn2402@usherbrooke.ca>
2022-06-06 15:29:42 -0400

    Children cutoff removed + source of data changed

--- obesity_figures.py
+++ obesity_figures.py
@@ -13,14 +13,13 @@ import matplotlib.pyplot as plt
 from os.path import join
 from pandas import read_csv
 from settings.paths import Paths
-from src.data.extraction.constants import AGE, PARTICIPANT, SEX, OBESITY_PERCENTILE, OBESITY_TARGET
+from src.data.extraction.constants import AGE, PARTICIPANT, SEX, OBESITY_TARGET
 from src.data.extraction.data_management import PetaleDataManager
-from src.data.processing.sampling import get_learning_one_data
 
 if __name__ == '__main__':
 
     # We read the data
-    df = read_csv(join(Paths.CSV_FILES, "obesity_predictions.csv"), index_col=0)
+    df = read_csv(join(Paths.CSV_FILES, "obesity_predictions_2.csv"), index_col=0)
 
     # We load the data that contains the sex and the age of the participants
     m = PetaleDataManager()
@@ -42,12 +41,10 @@ if __name__ == '__main__':
     plt.text(-1, 35.5, 'Women cutoff', fontdict={'size': 6})
     plt.axhline(y=25, linestyle='--', color='grey', lw=0.8)
     plt.text(-1, 25.5, 'Men cutoff', fontdict={'size': 6})
-    plt.axhline(y=OBESITY_PERCENTILE, linestyle='--', color='grey', lw=0.8)
-    plt.text(-1, OBESITY_PERCENTILE + 0.5, 'Children cutoff', fontdict={'size': 6})
     plt.plot(df.index, df['target'], 'x', color='grey', label='Targets')
-    plt.plot(men_df.index, men_df['Enet'], 'd', label='Lin. Reg. (w/o SNPs) (men)', color='#1f77b4')
-    plt.plot(women_df.index, women_df['Enet'], 'o', label='Lin. Reg. (w/o SNPs) (women)', color='#1f77b4')
-    plt.plot(children_df.index, children_df['Enet'], 's', label='Lin. Reg. (w/o SNPs) (children)', color='#1f77b4')
+    plt.plot(men_df.index, men_df['enetB'], 'd', label='Lin. Reg. (w/o SNPs) (men)', color='#1f77b4')
+    plt.plot(women_df.index, women_df['enetB'], 'o', label='Lin. Reg. (w/o SNPs) (women)', color='#1f77b4')
+    plt.plot(children_df.index, children_df['enetB'], 's', label='Lin. Reg. (w/o SNPs) (children)', color='#1f77b4')
     plt.plot(men_df.index, men_df['GGAE'], 'd', label='Lin. Reg. + GGAE (men)', color='orange')
     plt.plot(women_df.index, women_df['GGAE'], 'o', label='Lin. Reg. + GGAE (women)', color='orange')
     plt.plot(children_df.index, children_df['GGAE'], 's', label='Lin. Reg. + GGAE (children)', color='orange')

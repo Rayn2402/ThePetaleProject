cdcbb4631f924a7c79bd726ae056aa692ce378ca
Rayn2402 <rayn2402@usherbrooke.ca>
2022-07-11 11:49:53 -0400

    gene choice removed

--- scripts/obesity_experiments/apriori_experiment.py
+++ scripts/obesity_experiments/apriori_experiment.py
@@ -4,12 +4,12 @@ Filename: apriori_experiment.py
 Authors: Nicolas Raymond
 
 Description: This file is used to identify meaningful association
-             rules between categorical values and total body fat (%) quantiles.
+             rules between categorical values and total body fat (%) categories.
 
-Date of last modification : 2022/03/15
+Date of last modification : 2022/07/11
 """
 
-from os.path import dirname, exists, realpath, join
+from os.path import dirname, realpath
 from time import time
 
 import sys
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
     from settings.paths import Paths
     from src.data.extraction.data_management import PetaleDataManager
-    from src.data.processing.sampling import extract_masks, GeneChoice, get_obesity_data
+    from src.data.processing.sampling import extract_masks, get_obesity_data
     from src.utils.argparsers import apriori_argparser
     from src.utils.experiments import run_apriori_experiment
 
@@ -31,15 +31,15 @@ if __name__ == '__main__':
     # We save start time
     start = time()
 
-    # We first extract data
+    # We extract data
     manager = PetaleDataManager()
-    df, target, cont_cols, cat_cols = get_obesity_data(manager, genes=GeneChoice.ALL)
+    df, target, cont_cols, cat_cols = get_obesity_data(manager, genomics=True)
 
     # Extraction of masks
     masks = extract_masks(Paths.OBESITY_MASK, k=10, l=0)
 
     # We run apriori experiment
-    run_apriori_experiment(experiment_name='L1',
+    run_apriori_experiment(experiment_name='obesity',
                            df=df,
                            target=target,
                            cat_cols=cat_cols,

01d6834ff3d867d8d6d01468fe93022df98c7cd9
Rayn2402 <rayn2402@usherbrooke.ca>
2022-07-11 14:14:29 -0400

    obesity argparser modified

--- src/utils/argparsers.py
+++ src/utils/argparsers.py
@@ -73,16 +73,16 @@ def correct_and_smooth_parser():
     return arguments
 
 
-def fixed_hps_lae_experiment_parser():
+def obesity_experiment_parser():
     """
-    Creates a parser for the fixed hps lae experiments
+    Creates a parser for the obesity experiments
     """
     # Create a parser
-    parser = argparse.ArgumentParser(usage='\n python fixed_models_comparisons.py',
-                                     description="Runs all the experiments associated a dataset,"
-                                                 "using fixed hps")
+    parser = argparse.ArgumentParser(usage='\n python [experiment file].py',
+                                     description="Runs the experiments associated the obesity dataset,"
+                                                 "using manually selected hyperparameters")
 
-    # Nb inner split and nb outer split selection
+    # Nb of inner splits and nb of outer splits
     parser.add_argument('-k', '--nb_outer_splits', type=int, default=10,
                         help='Number of outer splits during the models evaluations')
     parser.add_argument('-l', '--nb_inner_splits', type=int, default=10,
@@ -90,50 +90,52 @@ def fixed_hps_lae_experiment_parser():
     parser.add_argument('-holdout', '--holdout', default=False, action='store_true',
                         help='If true, includes the holdout set data')
 
-    # Features selection
+    # Feature selection
     parser.add_argument('-class', '--classification', default=False, action='store_true',
                         help='If true, runs classification task instead of regression')
     parser.add_argument('-b', '--baselines', default=False, action='store_true',
                         help='If true, includes baselines in the features')
-    parser.add_argument('-gen0', '--custom_genes', nargs='*', type=str, default=[],
-                        help="Custom selection of genes to include.")
-    parser.add_argument('-gen1', '--genes_subgroup', default=False, action='store_true',
-                        help='If true, includes significant genes group in the features')
-    parser.add_argument('-gen2', '--all_genes', default=False, action='store_true',
-                        help='If true, includes genes in the features')
+    parser.add_argument('-gen', '--genomics', default=False, action='store_true',
+                        help='If true, includes all the genomic variables (SNPs)')
     parser.add_argument('-f', '--feature_selection', default=False, action='store_true',
                         help='If true, applies automatic feature selection')
 
     # Genes encoding parameter
     parser.add_argument('-share', '--embedding_sharing', default=False, action='store_true',
-                        help='If true, uses a single entity embedding layer for all genes'
-                             ' (currently only applies with genomic signature creation')
+                        help='If true, uses a single entity embedding layer for all SNPs'
+                             ' (only applied with genomic signature creation')
 
     # Models selection
     parser.add_argument('-enet', '--enet', default=False, action='store_true',
-                        help='If true, runs the enet experiment')
+                        help='If true, runs enet experiment')
     parser.add_argument('-mlp', '--mlp', default=False, action='store_true',
-                        help='If true, runs the mlp experiment')
+                        help='If true, runs mlp experiment')
     parser.add_argument('-rf', '--random_forest', default=False, action='store_true',
-                        help='If true, runs the random forest experiment')
+                        help='If true, runs random forest experiment')
     parser.add_argument('-xg', '--xg_boost', default=False, action='store_true',
-                        help='If true, runs the xgboost experiment')
+                        help='If true, runs xgboost experiment')
     parser.add_argument('-gat', '--gat', default=False, action='store_true',
-                        help='If true, runs the GraphAttentionNetwork experiment')
+                        help='If true, runs Graph Attention Network experiment')
     parser.add_argument('-gcn', '--gcn', default=False, action='store_true',
-                        help='If true, runs the GraphConvolutionalNetwork experiment')
+                        help='If true, runs Graph Convolutional Network experiment')
     parser.add_argument('-gge', '--gge', default=False, action='store_true',
-                        help='If true, runs the GeneGraphEncoder with enet experiment')
+                        help='If true, runs Gene Graph Encoder (with enet) experiment')
     parser.add_argument('-ggae', '--ggae', default=False, action='store_true',
-                        help='If true, runs the GeneGraphAttentionEncoder with enet experiment')
+                        help='If true, runs Gene GraphAttention Encoder (with enet) experiment')
 
-    # GAT graph construction parameters
+    # Training parameters
+    parser.add_argument('-epochs', '--epochs', type=int, default=500,
+                        help='Maximal number of epochs during training')
+    parser.add_argument('-patience', '--patience', type=int, default=50,
+                        help='Number of epochs allowed without improvement (for early stopping)')
+
+    # Graph construction parameters
     parser.add_argument('-w_sim', '--weighted_similarity', default=False, action='store_true',
                         help='If true, calculates patients similarities using weighted metrics')
     parser.add_argument('-cond_col', '--conditional_column', default=False, action='store_true',
-                        help='If true, uses the sex as conditional column in GAT construction')
+                        help='If true, uses the sex as a conditional column in graph construction')
     parser.add_argument('-deg', '--degree', nargs='*', type=str, default=[7],
-                        help="Maximum number of neighbors for each node in the graph")
+                        help="Maximum number of in-degrees for each node in the graph")
 
     # Gene encoding parameter
     parser.add_argument('-sign_size', '--signature_size', type=int, default=4,
@@ -141,20 +143,23 @@ def fixed_hps_lae_experiment_parser():
 
     # Self supervised learning experiments
     parser.add_argument('-ssl_ggae', '-ssl_ggae', default=False, action='store_true',
-                        help='If true, runs the self supervised learning with the GeneGraphAttentionEncoder')
+                        help='If true, runs self supervised learning with the Gene Graph Attention Encoder')
     parser.add_argument('-ssl_gge', '-ssl_gge', default=False, action='store_true',
-                        help='If true, runs the self supervised learning with the GeneGraphEncoder')
+                        help='If true, runs self supervised learning with the GeneGraphEncoder')
 
     # Activation of sharpness-aware minimization
-    parser.add_argument('-sam', '--enable_sam', default=False, action='store_true',
-                        help='If true, uses Sharpness-Aware Minimization Optimizer')
+    parser.add_argument('-rho', '--rho', type=float, default=0,
+                        help='Rho parameter of Sharpness-Aware Minimization (SAM) Optimizer.'
+                             'If >0, SAM is enabled')
 
     # Usage of predictions from another experiment
     parser.add_argument('-p', '--path', type=str, default=None,
                         help='Path leading to predictions of another model')
 
     # Seed
-    parser.add_argument('-seed', '--seed', type=int, default=1010710, help='Seed used during model evaluations')
+    parser.add_argument('-seed', '--seed', type=int, default=1010710,
+                        help='Seed used during model evaluations')
+
     arguments = parser.parse_args()
 
     # Print arguments
@@ -215,7 +220,7 @@ def VO2_experiment_parser():
     parser.add_argument('-ggae', '--ggae', default=False, action='store_true',
                         help='If true, runs Gene GraphAttention Encoder (with enet) experiment')
 
-    # Training parameter
+    # Training parameters
     parser.add_argument('-epochs', '--epochs', type=int, default=500,
                         help='Maximal number of epochs during training')
     parser.add_argument('-patience', '--patience', type=int, default=50,

32a77c04d2358293913e11c5ee5904100989cac4
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-07-12 16:39:40 -0400

    import lines moved

--- scripts/utils/generate_descriptive_analyses.py
+++ scripts/utils/generate_descriptive_analyses.py
@@ -4,22 +4,26 @@ Filename: generate_descriptive_analyses.py
 Author: Nicolas Raymond
         Mehdi Mitiche
 
-Description: This file stores the procedures that needs to be executed in order to extract descriptive tables
-             with information from all variables of a table.
+Description: Script that conducts a descriptive analysis of a table
 
-Date of last modification: 2022/04/26
+Date of last modification: 2022/07/12
 """
-from src.data.extraction.data_management import PetaleDataManager
+import sys
+from os.path import dirname, realpath
 
 if __name__ == '__main__':
 
-    # We build a PetaleDataManager that will help interacting with PETALE database
+    # Imports specific to project
+    sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
+    from src.data.extraction.data_management import PetaleDataManager
+
+    # We build a data manager
     data_manager = PetaleDataManager()
 
     # We retrieve the name of the table concerned
     TABLE = input("Write the table name : ")
 
-    # We proceed to the descriptive analysis of the variables of the table
+    # We proceed to the descriptive analysis of of the table
     stats = data_manager.get_table_stats(TABLE)
     print(stats, "\n\n")
 

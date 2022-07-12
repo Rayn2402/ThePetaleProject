b1e667bcbfd134faa9f2170d9f9e904860201481
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-07-12 14:26:49 -0400

    import lines + named both modified

--- scripts/post_analyses/obesity/get_classification_metrics.py
+++ scripts/post_analyses/obesity/get_classification_metrics.py
@@ -6,11 +6,16 @@ Author: Nicolas Raymond
 Description: Adds the sensitivity, specificity and bAcc scores to obesity experiments
              at a given path.
 """
+import sys
+from os.path import dirname, realpath
 from pandas import DataFrame
-from src.data.extraction.constants import AGE, CHILDREN_OBESITY_PERCENTILE, OBESITY, OBESITY_TARGET, SEX
-from src.utils.results_analysis import CLASS_PRED, get_classification_metrics, REG_PRED
 
 if __name__ == '__main__':
+
+    # Imports specific to project
+    sys.path.append(dirname(dirname(dirname(dirname(realpath(__file__))))))
+    from src.data.extraction.constants import AGE, CHILDREN_OBESITY_PERCENTILE, OBESITY, OBESITY_TARGET, SEX
+    from src.utils.results_analysis import CLASS_PRED, get_classification_metrics, REG_PRED
 
     # Class generation function
     def get_class_labels(df: DataFrame) -> DataFrame:
@@ -18,9 +23,9 @@ if __name__ == '__main__':
         Generates class labels from real valued predictions
 
         Args:
-            df: df with predictions
+            df: pandas dataframe with predictions
 
-        Returns: df
+        Returns: pandas dataframe
 
         """
         df.loc[(df[SEX] == 'Women') & (df[AGE] >= 18) & (df[REG_PRED] > 35), [CLASS_PRED]] = 1
@@ -28,8 +33,8 @@ if __name__ == '__main__':
 
         for sex, val in CHILDREN_OBESITY_PERCENTILE.items():
             for age, percentile in val.items():
-                filter = (df[SEX] == sex) & (df[AGE] >= age) & (df[AGE] < age + 0.5) & (df[REG_PRED] >= percentile)
-                df.loc[filter, [OBESITY]] = 1
+                filter_ = (df[SEX] == sex) & (df[AGE] >= age) & (df[AGE] < age + 0.5) & (df[REG_PRED] >= percentile)
+                df.loc[filter_, [OBESITY]] = 1
 
         return df
 

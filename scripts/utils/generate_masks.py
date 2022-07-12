863bd530bced6020439b813bbbe43fe43ad0e52e
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-07-12 16:54:18 -0400

    import lines moved

--- scripts/utils/generate_masks.py
+++ scripts/utils/generate_masks.py
@@ -1,32 +1,36 @@
 """
-Filename: generate_mask.py
+Filename: generate_masks.py
 
 Author: Nicolas Raymond
 
-Description: This file is used as a script to produce train, valid and test masks related to a learning table
+Description: Script used to produce train, valid and test masks related to a dataframe
 
-Date of last modification: 2021/11/15
+Date of last modification: 2022/07/12
 """
 import argparse
-
+import sys
 from json import dump
+from os.path import dirname, realpath
+
+sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 from src.data.processing.datasets import PetaleDataset
 from src.data.processing.sampling import RandomStratifiedSampler
 from src.data.extraction.constants import *
 from src.data.extraction.data_management import PetaleDataManager
 from src.data.extraction.helpers import retrieve_numerical_var
+from src.utils.argparsers import print_arguments
 
 
 def argument_parser():
     """
-    This function defines a parser to enable user to generate masks from a learning table
+    Creates a parser for the masks generation
     """
     # Create a parser
-    parser = argparse.ArgumentParser(usage='\n python generate_masks.py [table] [target]',
-                                     description="This program enables to generate stratified masks"
-                                                 " associated to a learning table.")
+    parser = argparse.ArgumentParser(usage='\n python generate_masks.py [table] [target] [...]',
+                                     description="Generates stratified masks"
+                                                 " associated from a dataframe.")
 
-    parser.add_argument('-tab', '--table', type=str, help="Name of the learning table")
+    parser.add_argument('-tab', '--table', type=str, help="Name of the postgresql table")
 
     parser.add_argument('-tc', '--target_column', type=str, help="Name of the column to use as target")
 
@@ -34,18 +38,19 @@ def argument_parser():
                         help='True if the target is categorical')
 
     parser.add_argument('-rc', '--removed_columns', nargs='*', type=str, default=[],
-                        help="Columns to remove from dataframe before creating mask")
+                        help="Columns to remove from the dataframe before creating the masks")
 
     parser.add_argument('-s', '--seed', type=int, default=SEED,
                         help=f"Seed value used to create masks (default = {SEED})")
 
     parser.add_argument('-vsize', '--validation_size', type=float, default=0.20,
-                        help="Percentage of data to use as valid set (default = 0.20)")
+                        help="Percentage of data used as valid set (default = 0.20)")
 
     parser.add_argument('-tsize', '--test_size', type=float, default=0.20,
-                        help="Percentage of data to use as test set (default = 0.20)")
+                        help="Percentage of data used as test set (default = 0.20)")
 
-    parser.add_argument('-fn', '--file_name', type=str, help="Name of the json file to store the masks")
+    parser.add_argument('-fn', '--file_name', type=str,
+                        help="Name of the json file used to store the masks")
 
     parser.add_argument('-a', '--alpha', type=float, default=SAMPLING_OUTLIER_ALPHA,
                         help=f"IQR multiplier used to check numerical variable range validity"
@@ -60,10 +65,7 @@ def argument_parser():
     arguments = parser.parse_args()
 
     # Print arguments
-    print("\nThe inputs are:")
-    for arg in vars(arguments):
-        print("{}: {}".format(arg, getattr(arguments, arg)))
-    print("\n")
+    print_arguments(arguments)
 
     return arguments
 
@@ -86,7 +88,9 @@ if __name__ == '__main__':
     # We create stratified mask according to the target columns
     cont_cols = [c for c in list(retrieve_numerical_var(df, []).columns.values) if c != args.target_column]
     cat_cols = [c for c in df.columns.values if c not in [PARTICIPANT, args.target_column] + cont_cols]
-    dataset = PetaleDataset(df, args.target_column, cont_cols=cont_cols, cat_cols=cat_cols,
+    dataset = PetaleDataset(df, args.target_column,
+                            cont_cols=cont_cols,
+                            cat_cols=cat_cols,
                             classification=args.categorical)
     rss = RandomStratifiedSampler(dataset, n_out_split=args.nb_out_split, n_in_split=args.nb_in_split,
                                   valid_size=args.validation_size, test_size=args.test_size,

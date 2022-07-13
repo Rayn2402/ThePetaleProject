2ed1265e9953c67c45b2173d790c611161308e86
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2022-07-13 11:04:37 -0400

    argparser docstring modified

--- scripts/utils/get_predictions_csv.py
+++ scripts/utils/get_predictions_csv.py
@@ -24,7 +24,7 @@ def paths_and_ids_parser():
     and a filename in which the predictions will be stored
     """
     # Create a parser
-    parser = ArgumentParser(usage='\n python3 file.py -p [path1 path2 ... ] -ids [id1 id2 ... ]',
+    parser = ArgumentParser(usage='\n python get_predictions_csv.py -p [path1 path2 ... ] -ids [id1 id2 ... ]',
                             description="Stores multiple path")
 
     parser.add_argument('-p', '--paths', nargs='*', type=str, help='List of paths')

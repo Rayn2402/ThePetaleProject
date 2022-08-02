0aeb67f33f4292e135ea35966fd4b5db224e5ebd
Rayn2402 <rayn2402@usherbrooke.ca>
2022-08-02 15:51:08 -0400

    New argparser

--- src/utils/argparsers.py
+++ src/utils/argparsers.py
@@ -5,7 +5,7 @@ Author: Nicolas Raymond
 
 Description: This file stores common argparser functions
 
-Date of last modification: 2022/02/08
+Date of last modification: 2022/08/02
 """
 
 import argparse
@@ -35,6 +35,27 @@ def apriori_argparser():
     arguments = parser.parse_args()
 
     # We show the arguments
+    print_arguments(arguments)
+
+    return arguments
+
+
+def data_source_parser():
+    """
+    Creates a parser for the selection of a data source
+    """
+    # Create a parser
+    parser = argparse.ArgumentParser(usage='\n python [file].py',
+                                     description="Enables to select data source")
+
+    # Data source
+    parser.add_argument('-from_csv', '--from_csv', default=False, action='store_true',
+                        help='If true, extract the data from the csv file instead'
+                             ' of the database.')
+
+    arguments = parser.parse_args()
+
+    # Print arguments
     print_arguments(arguments)
 
     return arguments
